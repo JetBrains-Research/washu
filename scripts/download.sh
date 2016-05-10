8b1a94a8790d5b2ff66f1b10a7df53c306121b82
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-05-10 23:14:20 +0300

    wget -O download to short names.

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,10 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
+# Download H3K4me3 data for Monocytes CD14+ from ENCODE and roadmapepigenomics
+# author oleg.shpynov@jetbrains.com
 
-# ENCODE
-wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR568/SRR568364/SRR568364.sra
-wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR568/SRR568365/SRR568365.sra
-# Roadmapepigenomics
-wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR787/SRR787515/SRR787515.sra
-wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR787/SRR787516/SRR787516.sra
+echo "Downloading ENCODE data"
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR568/SRR568364/SRR568364.sra -O SRR568364.sra
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR568/SRR568365/SRR568365.sra -O SRR568365.sra
 
-
+echo "Downloading Roadmapepigenomics data"
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR787/SRR787515/SRR787515.sra -O SRR787515.sra
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR787/SRR787516/SRR787516.sra -O SRR787516.sra
