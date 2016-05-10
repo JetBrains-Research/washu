22ce68e38697a2c2c21768de00eadc8b6fbfacab
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-05-10 16:37:02 -0500

    Load sratoolkit module

--- scripts/sra2fastq.sh
+++ scripts/sra2fastq.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # author oleg.shpynov@jetbrains.com
+# required modules: sratoolkit
 
 WORK_DIR=`pwd`
 SRA_FILE=$1
@@ -12,6 +13,9 @@ qsub << ENDINPUT
 #PBS -l nodes=1:ppn=1,walltime=2:00:00,vmem=6gb
 #PBS -j oe
 #PBS -q dque
+
+# Loading sratoolkit module
+module load sratoolkit
 
 cd $WORK_DIR
 fastq-dump --log-level err --dumpbase --gzip --outdir $WORK_DIR $SRA_FILE
