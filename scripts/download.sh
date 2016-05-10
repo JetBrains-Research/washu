fe2f8ed1d495c317a5bee988f6ccd90d4f564b4e
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-05-10 14:53:07 -0500

    Script to dowload ENCODE and roadmapepigenomics Monocytes CD14+; 2 donors; H3K4me3

---
+++ scripts/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# ENCODE
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR568/SRR568364/SRR568364.sra
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR568/SRR568365/SRR568365.sra
+# Roadmapepigenomics
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR787/SRR787515/SRR787515.sra
+wget -r ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR787/SRR787516/SRR787516.sra
+
+
