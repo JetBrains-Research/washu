aeed07cf16da443aa6ffbbf44fa9c9d79d8fddf8
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-05-11 04:23:16 +0300

    Cosmetics

--- scripts/makemehappy.sh
+++ scripts/makemehappy.sh
@@ -14,12 +14,13 @@ echo "Collecting tasks: sra2fastq"
 SRA_TASKS=""
 for FILE in $(find . -type f -name "*.sra")
 do :
+    NAME=${FILE%%.sra} # file name without extension
     if [ ! -n "$SRA_TASKS" ]; then
-        SRA_TASKS="sra2fastq_$FILE"
+        SRA_TASKS="sra2fastq_$NAME"
     else
-        SRA_TASKS="$SRA_TASKS,sra2fastq_$FILE"
+        SRA_TASKS="$SRA_TASKS,sra2fastq_$NAME"
     fi
 done
 
 echo "Waiting for tasks $SRA_TASKS to finish"
-qsub -hold_jid ${SRA_TASKS} -cwd ~/work/washu/scripts/makemehappy2.sh
+qsub -hold_jid $SRA_TASKS -cwd ~/work/washu/scripts/makemehappy2.sh
--- scripts/sra2fastq.sh
+++ scripts/sra2fastq.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # author oleg.shpynov@jetbrains.com
-# Submit multiple tasks with xargs:
-# find . -name "*.sra" -print0 | xargs -r0 -n1 sra2fastq.sh
 
 WORK_DIR=`pwd`
 SRA_FILE=$1
+NAME=${SRA_FILE%%.sra} # file name without sra extension
 
-echo "Converting sra to fastq-dump $SRA_FILE"
-
-qsub << ENDINPUT
+echo "Converting $SRA_FILE to fastq"
+if [ ! -f "$NAME.fastq" ]; then
+    qsub << ENDINPUT
 #!/bin/sh
-#PBS -N sra2fastq_$SRA_FILE
+#PBS -N sra2fastq_$NAME
 #PBS -l nodes=1:ppn=1,walltime=2:00:00,vmem=6gb
 #PBS -j oe
 #PBS -q dque
@@ -21,3 +20,4 @@ module load sratoolkit
 cd $WORK_DIR
 fastq-dump --split-3 --outdir $WORK_DIR $SRA_FILE
 ENDINPUT
+fi
