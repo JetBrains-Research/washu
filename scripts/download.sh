69aef4350a081e0b6603cf62467b9b6d536b6dc8
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-06-16 13:48:22 +0300

    no-clobber

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,6 +3,6 @@ Download data from GTAC facility
 # author oleg.shpynov@jetbrains.com
 
 echo "Downloading 1901_6 and 1901_7 data"
-wget -r https://htcf.wustl.edu/files/G3ex3Bdw/Oltz_1901_6/
-wget -r https://htcf.wustl.edu/files/G3ex3Bdw/Oltz_1901_7/
+wget -nc -r https://htcf.wustl.edu/files/G3ex3Bdw/Oltz_1901_6/
+wget -nc -r https://htcf.wustl.edu/files/G3ex3Bdw/Oltz_1901_7/
 find . -name *.fq.gz | xargs -i cp {} .
