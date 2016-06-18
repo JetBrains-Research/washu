af36d2449bf043cd6a3bb962778187f06af3af1a
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-06-18 14:58:41 +0300

    Added ATA data

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,11 +2,13 @@
 Download data from GTAC facility
 # author oleg.shpynov@jetbrains.com
 
+echo "Downloading 1903_1 and 1903_2 data"
+wget -nc -r https://htcf.wustl.edu/files/kQePwPdD/Oltz_1903_1/
+wget -nc -r https://htcf.wustl.edu/files/kQePwPdD/Oltz_1903_2/
+
 echo "Downloading 1901_6 and 1901_7 data"
 wget -nc -r https://htcf.wustl.edu/files/G3ex3Bdw/Oltz_1901_6/
 wget -nc -r https://htcf.wustl.edu/files/G3ex3Bdw/Oltz_1901_7/
-find . -name *.fq.gz | xargs -i mv {} .
-gunzip *.gz
 
-# Cleanup
-rm -r htcf.wustl.edu
+find . -name *.fq.gz | xargs -i cp {} .
+gunzip *.gz
