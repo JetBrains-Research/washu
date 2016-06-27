62474cc3f06b7f63ec6b15c204dd98802b40aefa
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-06-27 14:07:40 +0300

    Correct peaks number in report

--- macs2_logs.py
+++ macs2_logs.py
@@ -6,6 +6,7 @@ import sys
 import pandas as pd
 import os
 import re
+import subprocess
 
 help_message = 'Script to process macs2 logs summary.'
 
@@ -23,14 +24,14 @@ def macs2_logs(folder):
     PREDICTED_FRAGMENT = '.*predicted fragment length is'
     ALTERNATIVE_FRAGMENTS = '.*alternative fragment length\(s\) may be'
     print('Processing macs2 logs', folder)
-    df = pd.DataFrame(columns=['sample', 'tags', 'redundant_rate', 'peaks', 'fragment', 'alternatives'])
+    df = pd.DataFrame(columns=['sample', 'tags', 'redundant_rate', 'paired_peaks', 'fragment', 'alternatives'])
     for dirpath, dirs, files in os.walk(folder):
         for f in files:
             if 'macs' not in f or not re.search('.log$', f):
                 continue
             tags = ''
             rr = ''
-            peaks = ''
+            paired_peaks = ''
             fragment = ''
             alt_fragments = ''
             for line in open(dirpath + '/' + f, 'r'):
@@ -39,13 +40,22 @@ def macs2_logs(folder):
                 if re.search(REDUNDANT_RATE, line):
                     rr = re.sub(REDUNDANT_RATE, '', line).strip()
                 if re.search(PAIRED_PEAKS, line):
-                    peaks = re.sub(PAIRED_PEAKS, '', line).strip()
+                    paired_peaks = re.sub(PAIRED_PEAKS, '', line).strip()
                 if re.search(PREDICTED_FRAGMENT, line):
                     fragment = re.sub(PREDICTED_FRAGMENT, '', line).replace('bps', '').strip()
                 if re.search(ALTERNATIVE_FRAGMENTS, line):
                     alt_fragments = re.sub(ALTERNATIVE_FRAGMENTS, '', line).replace('bps', '').strip()
-
-            df.loc[len(df)] = (f, tags, rr, peaks, fragment, alt_fragments)
+            df.loc[len(df)] = (f, tags, rr, paired_peaks, fragment, alt_fragments)
+    # Collect line sizes of peaks file
+    wcs = []
+    for dirpath, dirs, files in os.walk(folder):
+        for f in files:
+            if not re.search('.bed$', f):
+                continue
+            wcs.append(subprocess.Popen(['wc', '-l', folder + '/' + f], stdout=subprocess.PIPE).communicate()[0].decode(
+                'utf-8').strip())
+    df['peaks'] = df['sample'].map(lambda x: [wc.rpartition(' ')[0] for wc in wcs if x.rpartition('_macs')[0] in wc][0])
+    print(df)
     return df
 
 
