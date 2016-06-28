93fb1b20d03bfd33e5f9cd6e992fd1cc136d4015
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-06-28 13:56:46 +0300

    BWA indexes

--- scripts/genome_indices.sh
+++ scripts/genome_indices.sh
@@ -28,6 +28,13 @@ if [ ! -f "${GENOME}.2bit" ]; then
     chmod a+r *
 fi
 
+echo "Check fa reference"
+if [ ! -f "${GENOME}.fa" ]; then
+    echo "Convert 2bit to fa reference"
+    ~/twoBitToFa/twoBitToFa ${GENOME}.2bit ${GENOME}.fa
+    chmod a+r *
+fi
+
 echo "Check bowtie indexes ${GENOME}"
 if [ ! -f "$GENOME.1.ebwt" ]; then
     QSUB_ID=$(qsub << ENDINPUT
@@ -43,6 +50,27 @@ module load bowtie
 # This is necessary because qsub default working dir is user home
 cd ${FOLDER}
 bowtie-build $(find . -type f -name "*.fa" -printf '%P\n' | paste -sd "," -) ${GENOME}
+ENDINPUT
+)
+    wait_complete ${QSUB_ID}
+    check_logs
+fi
+
+echo "Check bwa indexes ${GENOME}"
+if [ ! -f "$GENOME.fa.bwt" ]; then
+    QSUB_ID=$(qsub << ENDINPUT
+#!/bin/sh
+#PBS -N bwa_indexes_${GENOME}
+#PBS -l nodes=1:ppn=8,walltime=24:00:00,vmem=16gb
+#PBS -j oe
+#PBS -o ${FOLDER}/${GENOME}_bwa_indexes.log
+
+# Load module
+module load bwa
+
+# This is necessary because qsub default working dir is user home
+cd ${FOLDER}
+bwa index -a bwtsw ${GENOME}.fa
 ENDINPUT
 )
     wait_complete ${QSUB_ID}
