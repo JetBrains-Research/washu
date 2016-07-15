af54358386238cd03136b00316f3662882379f2d
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-07-15 13:26:36 +0300

    Fixed problem with not sorted clip file in bigwig processing

--- bdg2bw.sh
+++ bdg2bw.sh
@@ -9,17 +9,18 @@ which bedClip &>/dev/null || { echo "bedClip not found! Download: <http://hgdown
  
 # end of checking
  
-if [ $# -lt 2 ];then
+if [ $# -lt 2 ]; then
     echo "Need 2 parameters! <bedgraph> <chrom info>"
-    exit
+    exit 1
 fi
  
 F=$1
 G=$2
  
 bedtools slop -i ${F} -g ${G} -b 0 | bedClip stdin ${G} ${F}.clip
+# Fix problem with not sorted clip file
+LC_COLLATE=C sort -k1,1 -k2,2n ${F}.clip > ${F}.sort.clip
+bedGraphToBigWig ${F}.sort.clip ${G} ${F/bdg/bw}
  
-bedGraphToBigWig ${F}.clip ${G} ${F/bdg/bw}
- 
-rm -f ${F}.clip
+rm -f ${F}*.clip
 
