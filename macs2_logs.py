fc572dac33db6fdaabf2e49d3d74aed9c87f4eef
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-07-26 15:01:36 +0300

    Compute FRiP automatically

--- macs2_logs.py
+++ macs2_logs.py
@@ -36,30 +36,42 @@ def macs2_logs(folder):
             alt_fragments = ''
             for line in open(dirpath + '/' + f, 'r'):
                 if re.search(TAGS, line):
-                    tags = re.sub(TAGS, '', line).strip()
+                    tags = int(re.sub(TAGS, '', line).strip())
                 if re.search(REDUNDANT_RATE, line):
-                    rr = re.sub(REDUNDANT_RATE, '', line).strip()
+                    rr = float(re.sub(REDUNDANT_RATE, '', line).strip())
                 if re.search(PAIRED_PEAKS, line):
-                    paired_peaks = re.sub(PAIRED_PEAKS, '', line).strip()
+                    paired_peaks = int(re.sub(PAIRED_PEAKS, '', line).strip())
                 if re.search(PREDICTED_FRAGMENT, line):
-                    fragment = re.sub(PREDICTED_FRAGMENT, '', line).replace('bps', '').strip()
+                    fragment = int(re.sub(PREDICTED_FRAGMENT, '', line).replace('bps', '').strip())
                 if re.search(ALTERNATIVE_FRAGMENTS, line):
                     alt_fragments = re.sub(ALTERNATIVE_FRAGMENTS, '', line).replace('bps', '').strip()
             df.loc[len(df)] = (f, tags, rr, paired_peaks, fragment, alt_fragments)
-    # Collect line sizes of peaks file
-    wcs = []
+
+    # Lines count data
+    lcs = []
+    # FRiP data
+    rips = []
     for dirpath, dirs, files in os.walk(folder):
         for f in files:
-            if not re.search('.(bed|broadPeak|narrowPeak)$', f):
-                continue
-            wcs.append(subprocess.Popen(['wc', '-l', folder + '/' + f], stdout=subprocess.PIPE).communicate()[0].decode(
-                'utf-8').strip())
+            if re.search('.(bed|broadPeak|narrowPeak)$', f):
+                lcs.append(
+                    subprocess.Popen(['wc', '-l', folder + '/' + f], stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip())
+            if re.search('.txt$', f):
+                rips.append([line.rstrip('\n') for line in open(folder + '/' + f)][0])
 
-    def wc_find(x):
-        rec = [wc.rpartition(' ')[0] for wc in wcs if x.rpartition('_macs')[0] in wc]
-        return 'NaN' if len(rec) == 0 else rec[0]
+    def lc_find(x):
+        """Lines count"""
+        rec = [lc.rpartition(' ')[0] for lc in lcs if x.rpartition('_macs')[0] in lc]
+        return 0 if len(rec) == 0 else int(rec[0])
 
-    df['peaks'] = df['sample'].map(lambda x: wc_find(x))
+    def rip_find(x):
+        """Read in Peaks"""
+        rec = [rip.rpartition('\t')[2] for rip in rips if x.rpartition('_macs')[0] in rip]
+        return 0 if len(rec) == 0 else int(rec[0])
+
+    df['peaks'] = df['sample'].map(lambda x: lc_find(x))
+    df['rip'] = df['sample'].map(lambda x: rip_find(x))
+    df['frip'] = 100 * df['rip'] / df['tags']
     return df
 
 
