33cbbfd2058a89f05dbe82de5abee73ebc86ba81
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2016-10-12 13:50:13 -0500

    Fix LCS

--- scripts/macs2_find_input.py
+++ scripts/macs2_find_input.py
@@ -17,18 +17,33 @@ def usage():
 
 
 def lcs(x, y):
-    """Finds longest common subsequence"""
+    """
+    Finds longest common subsequence
+    Code adopted from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_subsequence#Python
+    """
     m = len(x)
     n = len(y)
     # An (m+1) times (n+1) matrix
     c = [[0] * (n + 1) for _ in range(m + 1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if x[i-1] == y[j-1]:
-                c[i][j] = c[i-1][j-1] + 1
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:
+                c[i][j] = c[i - 1][j - 1] + 1
             else:
-                c[i][j] = max(c[i][j-1], c[i-1][j])
-    return c
+                c[i][j] = max(c[i][j - 1], c[i - 1][j])
+
+    def back_track(i, j):
+        if i == 0 or j == 0:
+            return ""
+        elif x[i - 1] == y[j - 1]:
+            return back_track(i - 1, j - 1) + x[i - 1]
+        else:
+            if c[i][j - 1] > c[i - 1][j]:
+                return back_track(i, j - 1)
+            else:
+                return back_track(i - 1, j)
+
+    return len(back_track(m, n))
 
 
 def process(file):
@@ -41,6 +56,7 @@ def process(file):
 
     def sort_function(x):
         return lcs(file, x)
+
     extension = os.path.splitext(file)[1]
     inputs = [x for x in f if 'input' in x and extension in x]
     if len(inputs) > 0:
@@ -50,6 +66,12 @@ def process(file):
 
 
 def main():
+    # TODO[oleg] test me!
+    if (lcs('UW_CD14_RO01746_k4me3_1_1_ENCFF001FYS.fastq', 'UW_CD14_input_ENCFF001HUV.fastq') <
+            lcs('UW_CD14_RO01746_k4me3_1_1_ENCFF001FYS.fastq', 'Broad_CD14_2_input_ENCFF000CCW.fastq')):
+        print("Regression found, add properly testing")
+        sys.exit(1)
+
     argv = sys.argv
     opts, args = getopt.getopt(argv[1:], "h", ["help"])
 
