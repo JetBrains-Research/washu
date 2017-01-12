716a383a81eca3f55f354804e0064aa5d1c40157
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-01-13 00:06:40 +0300

    Convert chromhmm track from bb to bed

--- chromhmm_stats.sh
+++ chromhmm_stats.sh
@@ -2,6 +2,9 @@
 
 # Script to analyze chromhmm states composition of given file
 # author: oleg.shpynov@jetbrains.com
+#
+# ChromHMM precomputed markup is available at:
+# https://www.encodeproject.org/report/?type=Annotation&software_used.software.name=chromhmm
 
 # Example:
 # > chromhmm_states.sh k27ac.diff cd14_chromhmm.bed
@@ -14,14 +17,22 @@
 which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
 
 if [ $# -lt 2 ]; then
-    echo "Need 2 parameters! <BED_FILE> <CHROMHMM_MARKUP_FILE>"
+    echo "Need 2 parameters! <BED_FILE> <CHROMHMM_MARKUP_FILE.BED>"
     exit 1
 fi
 
 FILE=$1
 CHROMHMM=$2
 
+if [ ! $CHROMHMM =~ *.bed ]; then
+    which bigBedToBed &>/dev/null || { echo "bigBedToBed not found! Download: http://hgdownload.cse.ucsc.edu/admin/exe/"; exit 1; }
+    NAME=${CHROMHMM%%.*}
+    echo "Bed file required, found bigBed. Converting to $CHROMHMM.bed"
+    bigBedToBed $FILE $CHROMHMM.bed
+    CHROMHMM=$CHROMHMM.bed
+fi
+
 # Compute intersection length by chromhmm state
 bedtools intersect -a $FILE -b $CHROMHMM -wa -wb |\
- awk -v COLS=$(head -1 $FILE | awk '{ print NF }') '{ tot[$(4+COLS)]+=(($(3+COLS)<$3?$(3+COLS):$3)-($(2+COLS)>$2?$(2+COLS):$2)) } END { for (i in tot) print i"\t"tot[i] }' |\
-sort
+ awk -v COLS=$(head -1 $FILE | awk '{ print NF }') '{ tot[$(4+COLS)]+=1 } END { for (i in tot) print i"\t"tot[i] }' |\
+ sort
