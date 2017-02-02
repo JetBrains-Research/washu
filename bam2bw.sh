d7c905c24a13fb3d899c486bbf99ba45260dfbae
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-02-02 19:44:38 +0300

    Merged bam2bw.sh into a single script

--- bam2bw.sh
+++ bam2bw.sh
@@ -3,8 +3,10 @@
 # Original code: http://crazyhottommy.blogspot.ru/2014/10/convert-bam-file-to-bigwig-file-and.html
 # author: oleg.shpynov@jetbrains.com
 
-# Check tool.
-which genomeCoverageBed &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
+# Check tools
+which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
+which bedGraphToBigWig &>/dev/null || { echo "bedGraphToBigWig not found! Download: <http://hgdownload.cse.ucsc.edu/admin/exe/>"; exit 1; }
+which bedClip &>/dev/null || { echo "bedClip not found! Download: <http://hgdownload.cse.ucsc.edu/admin/exe/>"; exit 1; }
 
 if [ $# -lt 2 ]; then
     echo "Need 2 parameters! <BAM> <chrom.sizes>"
@@ -23,3 +25,10 @@ NAME=${BAM%%.bam}
 genomeCoverageBed -ibam $BAM -bg -g ${CHROM_SIZES} > ${NAME}.bdg
 bash ~/work/washu/bdg2bw.sh ${NAME}.bdg ${CHROM_SIZES}
 
+# Remove coordinates outside chromosome sizes
+bedtools slop -i ${NAME}.bdg -g ${CHROM_SIZES} -b 0 | bedClip stdin ${CHROM_SIZES} ${NAME}.bdg.clip
+# Fix problem with not sorted clip file
+LC_COLLATE=C sort -k1,1 -k2,2n -k3,3n ${NAME}.bdg.clip > ${NAME}.bdg.sort.clip
+bedGraphToBigWig ${NAME}.bdg.sort.clip ${CHROM_SIZES} ${NAME}.bw
+ 
+rm -f ${NAME}.bdg*.clip
