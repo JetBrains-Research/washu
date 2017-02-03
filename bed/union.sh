6fde3d054e62b4b00120b61917a5cd4b10bccedc
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-02-03 20:41:48 +0300

    Cleanup

--- bed/union.sh
+++ bed/union.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# This script is used to compute intersection of peaks for given list of files.
+# This script is used to compute union of peaks for given list of files.
 #
 # What happens:
 # - Filter out unknown contigs and Y chromosome peaks
 # - Two peaks aver overlapping if they share at least one nucleotide
-# - Prints only peaks that overlap in all files (merged)
+# - Prints only peaks that exist at least in one file.
+#       4th column indicates tracks, parents of each peak in union.
 #
 # author Oleg Shpynov (oleg.shpynov@jetbrains.com)
 
 which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
->&2 echo "intersect: $@"
+>&2 echo "union: $@"
 
 TMP="_to_merge.bed"
 if [ -f ${TMP} ]; then
