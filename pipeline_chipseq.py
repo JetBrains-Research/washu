086970d7858f51e55dbc07e8b22a0ea45f80653e
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-03-07 10:22:53 +0300

    Do not recompute if MACS resulting folder exists

--- pipeline_chipseq.py
+++ pipeline_chipseq.py
@@ -69,14 +69,18 @@ move_forward(WORK_DIR, "_bws", ["*.bw", "*.bdg", "*bw.log"], copy_only=True)
 
 # Batch macs2 with different peak calling procedures settings
 for Q in [0.01, 0.001, 0.1]:
-    run_bash("macs2.sh", WORK_DIR, GENOME, str(Q))
-    move_forward(WORK_DIR, "_macs_{}".format(Q), ["*{}*".format(Q)], copy_only=True)
-    process_macs2_logs(WORK_DIR + "_macs_{}".format(Q))
+    macs_suffix = "_macs_{}".format(Q)
+    if not os.path.exists(WORK_DIR + macs_suffix):
+        run_bash("macs2.sh", WORK_DIR, GENOME, str(Q))
+        move_forward(WORK_DIR, macs_suffix, ["*{}*".format(Q)], copy_only=True)
+        process_macs2_logs(WORK_DIR + macs_suffix)
 
 for Q in [0.01, 0.001, 0.1]:
-    run_bash("macs2_broad.sh", WORK_DIR, GENOME, str(Q))
-    move_forward(WORK_DIR, "_macs_broad_{}".format(Q), ["*{}*".format(Q)], copy_only=True)
-    process_macs2_logs(WORK_DIR + "_macs_broad_{}".format(Q))
+    macs_broad_suffix = "_macs_broad_{}".format(Q)
+    if not os.path.exists(WORK_DIR + macs_broad_suffix):
+        run_bash("macs2_broad.sh", WORK_DIR, GENOME, str(Q))
+        move_forward(WORK_DIR, macs_broad_suffix, ["*{}*".format(Q)], copy_only=True)
+        process_macs2_logs(WORK_DIR + macs_broad_suffix)
 
 # # Batch macs14 with different peak calling procedures settings
 # # P = 1e-5 is default for MACS14
