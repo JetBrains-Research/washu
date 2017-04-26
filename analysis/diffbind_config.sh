4c018ab90f9504268885c69d1fbf46b14d7f5e5f
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-04-26 12:17:01 +0300

    More robust input, i.e. provide READS and PEAKS folders manually

--- analysis/diffbind_config.sh
+++ analysis/diffbind_config.sh
@@ -4,34 +4,30 @@
 
 >&2 echo "diffbind_config: $@"
 if [ $# -lt 2 ]; then
-    echo "Need 2 parameters! <WORKING_FOLDER> <NAME> <Q>"
-    echo "Example: scratch/artyomov_lab_aging/Y10OD10/chipseq/processed k27ac_10vs10"
-    echo "Necessary folders: \${NAME}_bams, \${NAME}_bams_macs_broad_\$Q"
+    echo "Need 2 parameters! <READS_DIR> <PEAKS_DIR>"
+    echo "Example: "
+    echo "  bash diffbind_config.sh scratch/artyomov_lab_aging/Y10OD10/chipseq/processed/k4me1_10vs10_reseq_bams\\"
+    echo "    scratch/artyomov_lab_aging/Y10OD10/chipseq/processed/k4me1_10vs10_reseq_bams_macs_broad_0.1"
     exit 1
 fi
-WORK_DIR=$1
-NAME=$2
-Q=$3
 
-cd $WORK_DIR
-READS_DIR=${NAME}_bams
-PEAKS_DIR=${NAME}_bams_macs_broad_${Q}
-
->&2 echo "WORK_DIR: $WORK_DIR"
-if [[ ! -d ${WORK_DIR} ]]; then
-    echo "Missing folder ${WORK_DIR}"
-    exit 1
-fi
+READS_DIR=$1
 >&2 echo "READS_DIR: $READS_DIR"
 if [[ ! -d ${READS_DIR} ]]; then
     echo "Missing folder ${READS_DIR}"
     exit 1
 fi
+
+PEAKS_DIR=$2
 >&2 echo "PEAKS_DIR: $PEAKS_DIR"
 if [[ ! -d ${PEAKS_DIR} ]]; then
     echo "Missing folder ${PEAKS_DIR}"
     exit 1
 fi
+
+# To absolute paths
+READS_DIR=$(readlink -f $READS_DIR)
+PEAKS_DIR=$(readlink -f $PEAKS_DIR)
 
 # Start with reads, so that we can move outliers to separate folders and process only valid data
 READS_FILES=$(find $READS_DIR -name "*.bam" | grep -v input | sort)
@@ -52,5 +48,5 @@ for R in $READS_FILES; do
     >&2 echo "CONTROL: $CONTROL"
     PEAK=$(ls $PEAKS_DIR/${SAMPLE}*.xls)
     >&2 echo "PEAK: $PEAK"
-    echo "$SAMPLE,CD14,Age,$CONDITION,$REPLICATE,$WORK_DIR/$READ,${CONDITION}_pooled,$WORK_DIR/$CONTROL,$WORK_DIR/${PEAK},macs"
+    echo "$SAMPLE,CD14,Age,$CONDITION,$REPLICATE,$READ,${CONDITION}_pooled,$CONTROL,${PEAK},macs"
 done
