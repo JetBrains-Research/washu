92d1ec68a3bfbbda48e4ffeb3e4a80073f78ec47
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-04-28 16:25:39 +0300

    Plot all possible graphics with and without contrast variable

--- analysis/diffbind.R
+++ analysis/diffbind.R
@@ -5,20 +5,20 @@
 installed <- rownames(installed.packages())
 
 require_or_install <- function(..., bioc = FALSE) {
-  for (p in list(...)) {
-    if (!(p %in% installed)) {
-      if (bioc) {
-        if (!exists("biocLite")) {
-          source("http://bioconductor.org/biocLite.R")
+    for (p in list(...)) {
+        if (! (p %in% installed)) {
+            if (bioc) {
+                if (! exists("biocLite")) {
+                    source("http://bioconductor.org/biocLite.R")
+                }
+                biocLite(p)
+            } else {
+                install.packages(p, repos = "http://cran.us.r-project.org")
+            }
         }
-        biocLite(p)
-      } else {
-        install.packages(p, repos = "http://cran.us.r-project.org")
-      }
-    }
 
-    require(p, character.only = TRUE)
-  }
+        require(p, character.only = TRUE)
+    }
 }
 
 require_or_install("DiffBind")
@@ -26,41 +26,62 @@ require_or_install("ggplot2")
 require_or_install("stringr")
 
 main <- function(path) {
-  write(paste("DiffBind version: ", packageVersion("DiffBind")))
-  write(paste("Processing file", path))
-  result_pdf = str_replace(path, ".csv", "_result.pdf")
-  pdf(file=result_pdf)
-  yo=dba(sampleSheet=path)
-  yo = dba.count(yo)
-  # Plot histogram
-  plot(yo)
-  # Analyze contrast factor and compute difference
-  yo = dba.contrast(yo)
-  yo = dba.analyze(yo)
-  # Print number of overlap peaks by donors
-  olap.rate=dba.overlap(yo,mode=DBA_OLAP_RATE)
-  plot(olap.rate,type="b", ylab="# peaks", xlab="Overlap at least this many peaksets")
-  # PCA, MA plots
-  dba.plotPCA(yo)
-  dba.plotMA(yo)
-  # Save difference to resulting csv file
-  db = dba.report(yo)
-  result_csv = str_replace(path, ".csv", "_result.csv")
-  write.table(db, result_csv, sep = ",", row.names = FALSE)
-  write(paste("Saved", result_csv))
-  # IMPORTANT: plot difference histogram on the last step as it fails if there is no difference
-  dba.plotBox(yo)
-  dev.off()
-  write(paste("Saved plots", result_pdf))
+    write(paste("DiffBind version: ", packageVersion("DiffBind")))
+    write(paste("Processing file", path))
+    yo = dba(sampleSheet = path)
+    yo = dba.count(yo)
+
+    # Plot histogram
+    pdf(file = str_replace(path, ".csv", "_histogram.pdf"))
+    plot(yo)
+    dev.off()
+
+    # Analyze contrast factor and compute difference
+    yo = dba.contrast(yo)
+    yo = dba.analyze(yo)
+
+    # Print number of overlap peaks by donors
+    pdf(file = str_replace(path, ".csv", "_overlap.pdf"))
+    overlap_rate = dba.overlap(yo, mode = DBA_OLAP_RATE)
+    plot(overlap_rate, type = "b", ylab = "# peaks", xlab = "Overlap at least this many peaksets")
+    dev.off()
+
+    # PCA, MA plots
+    pdf(file = str_replace(path, ".csv", "_pca.pdf"))
+    dba.plotPCA(yo)
+    dev.off()
+
+    pdf(file = str_replace(path, ".csv", "_ma.pdf"))
+    dba.plotMA(yo)
+    dev.off()
+
+    # Save difference to resulting csv file
+    db = dba.report(yo)
+    result_csv = str_replace(path, ".csv", "_result.csv")
+    write.table(db, result_csv, sep = ",", row.names = FALSE)
+    write(paste("Saved", result_csv))
+
+    # IMPORTANT: plot difference on the last step as it fails if there is no difference
+    pdf(file = str_replace(path, ".csv", "_difference_histogram.pdf"))
+    plot(yo, contrast = 1)
+    dev.off()
+
+    pdf(file = str_replace(path, ".csv", "_difference_pca.pdf"))
+    dba.plotPCA(yo, contrast = 1)
+    dev.off()
+
+    pdf(file = str_replace(path, ".csv", "difference.pdf"))
+    dba.plotBox(yo)
+    dev.off()
 }
 
-if (!interactive()) {
-  args <- commandArgs(TRUE)
-  if (length(args) != 1) {
-    write("Usage: [executable] diffbind.csv", stderr())
-    q(status = 1)
-  } else {
-    do.call(main, as.list(args))
-    warnings()
-  }
+if (! interactive()) {
+    args <- commandArgs(TRUE)
+    if (length(args) != 1) {
+        write("Usage: [executable] diffbind.csv", stderr())
+        q(status = 1)
+    } else {
+        do.call(main, as.list(args))
+        warnings()
+    }
 }
