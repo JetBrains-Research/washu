e6112d21c0010a1f544bb4d7e831470be87724ee
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-05-29 11:38:02 +0300

    Comment on sicer output files.

--- scripts/sicer.sh
+++ scripts/sicer.sh
@@ -94,6 +94,15 @@ cd ${INPUT_FOLDER}
 
 SICER.sh ${INPUT_FOLDER} ${FILE_BED} ${INPUT_BED} ${OUT_FOLDER} ${GENOME} 1 200 150 ${EFFECTIVE_GENOME_FRACTION} 600 ${FDR}
 cp -f ${OUT_FOLDER}/* ${WORK_DIR}
+
+# Output files:
+# -normalized.wig. This file is in WIG format and could be uploaded to the UCSC genome browser for visualization of the ChIP library with redundancy-removed but before island-filtering (ES_H3K27me3-1-removed.bed) with desired window size.
+# .scoreisland. This file stores all identified islands with respective scores and could be used to evaluate the choice of gap size.
+# -islands-summary. This 8-column file is a summary of all islands identified in the ChIP library. The format is chromosome, start, end, read count in ChIP library, read count in control library, p-value, fold change, FDR).
+# -island.bed. This file has 4 columns and contains information about all identified ChIP-enriched region information under filtering parameters (window size 200, gap size 600, FDR cutoff 1e-3). The 4 columns are chromosome, start, end, and read count in ChIP library.
+# -islandfiltered.bed. This file is in BED format and contains all reads that are within significant islands.
+# -islandfiltered-normalized.wig. This file is in WIG format and could be uploaded directly to UCSC genome browser for visualization of the island-filtered ChIP library.
+# -removed.bed. This file contains reads after redundancy removed.
 ENDINPUT
 )
     echo "FILE: ${FILE}; JOB: ${QSUB_ID}"
