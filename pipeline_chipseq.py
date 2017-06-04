fc29aeb846dcbd490d19df88e475e37cb6801d28
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-06-05 00:53:48 +0300

    Pileup.sh is not useful itself, create BigWigs for deduplicated files with BDG coverage

--- pipeline_chipseq.py
+++ pipeline_chipseq.py
@@ -90,9 +90,9 @@ move_forward(WORK_DIR, WORK_DIR + "_rpkms", ["*.bw", "*rpkm.log"], copy_only=Tru
 run_bash("remove_duplicates.sh", WORK_DIR)
 move_forward(WORK_DIR, WORK_DIR + "_unique", ["*_unique*", "*_metrics.txt", "*duplicates.log"], copy_only=True)
 
-# Build BED pileup for deduplicated files
-run_bash("pileup.sh", WORK_DIR + "_unique")
-move_forward(WORK_DIR + "_unique", WORK_DIR + "_unique_bed", ["*pileup*", "*.bed"], copy_only=True)
+# # Batch BigWigs and BDG coverage for deduplicated files
+run_bash("bigwig.sh", WORK_DIR + "_unique")
+move_forward(WORK_DIR + "_unique", WORK_DIR + "_unique_bws", ["*.bw", "*.bdg", "*bw.log"], copy_only=True)
 
 # # Batch subsampling
 # run_bash("subsample.sh", WORK_DIR, str(READS))
