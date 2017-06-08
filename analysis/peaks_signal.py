76e6e5620c24f2a5174d9976cdb9dd7d87b26fe0
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-06-07 19:40:31 -0500

    Don't use /tmp for computations, use local folder

--- analysis/peaks_signal.py
+++ analysis/peaks_signal.py
@@ -2,7 +2,7 @@
 # Author: oleg.shpynov@jetbrains.com
 
 import argparse
-import tempfile
+import os
 from collections import namedtuple
 import numpy as np
 import pandas as pd
@@ -12,26 +12,25 @@ Record = namedtuple('Record', ['name', 'bdg'])
 
 
 def process(regions, records, out):
-    with tempfile.NamedTemporaryFile(mode='w', suffix='.bed3', prefix='regions', delete=True) as tmp1:
-        regions3 = tmp1.name
-        Bed(regions).save3(regions3)
+    print('Save regions as BED3 format {}.bed3'.format(regions))
+    regions3 = '{}.bed3'.format(regions)
+    Bed(regions).save3(regions3)
 
-        with tempfile.NamedTemporaryFile(mode='w', suffix='.bed', prefix='intersect', delete=False) as tmp2:
-            intersection_path = tmp2.name
-            print('Compute summary intersection file {}'.format(intersection_path))
-            run([['bedtools', 'intersect', '-wa', '-wb',
-                  '-a', regions3,
-                  '-b', *[r.bdg for r in records],
-                  '-names', *[r.name for r in records]],
-                 ['awk', '-v', "OFS=\\t", '{print($1,$2,$3,$4,$8)}']],
-                stdout=tmp2)
-
-            print('Intersection file: {}'.format(intersection_path))
-            compute_signal(intersection_path, out, records)
+    intersection_path = '{}.intersection.tsv'.format(out)
+    print('Compute summary intersection file {}'.format(intersection_path))
+    cmd = [['bedtools', 'intersect', '-wa', '-wb', '-a', regions3, '-b', *[r.bdg for r in records], '-names',
+            *[r.name for r in records], '-sorted'], ['awk', '-v', "OFS=\\t", '{print($1,$2,$3,$4,$8)}']]
+    print(' | '.join([' '.join(c) for c in cmd]) + ' > ' + intersection_path)
+    with open("Output.txt", "w") as intersection:
+        run(cmd, stdout=intersection)
+        compute_signal(intersection_path, out, records)
+    # Cleanup
+    os.remove(regions3)
+    os.remove(intersection_path)
 
 
 def compute_signal(intersection_path, out, records):
-    print('Compute summary signal')
+    print('Compute summary signal by {}'.format(intersection_path))
     intersection = pd.read_csv(intersection_path, names=('chr', 'start', 'end', 'name', 'coverage'), sep='\t')
     coverage = intersection.groupby(['chr', 'start', 'end', 'name'], as_index=False).aggregate(np.sum)
     # Pivot by names
@@ -56,15 +55,14 @@ def main():
     parser.add_argument('--bedgraphs', metavar='name bdg', type=str, nargs='+',
                         help='Bedgraph files to be processed in format: name bdg [name bdg ...]', required=True)
     args = parser.parse_args()
-    print('ARGS: {}'.format(args))
     regions = args.regions
     print('Regions: {}'.format(regions))
     out = args.out
-    print('Out suffix: {}'.format(out))
+    print('Output suffix: {}'.format(out))
     # Load records
     it = iter(args.bedgraphs)
     records = [Record(*z) for z in zip(it, it)]
-    print('Records:\n{}'.format(records))
+    print('Records: {}'.format(records))
     process(regions, records, out)
 
 
