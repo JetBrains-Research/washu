09f81350b660ff00873c7b3a657f1794e2d695f4
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-07-03 15:31:50 +0300

    download geo data using bash script

--- pipeline_tf.py
+++ pipeline_tf.py
@@ -9,6 +9,7 @@
 # 6. macs2
 
 import os
+import itertools
 
 import click
 import pandas as pd
@@ -36,7 +37,8 @@ def cli(out, data):
     #################
     WORK_DIR = out
     GENOME = "hg19"
-    INDEXES = os.path.join("/scratch/artyomov_lab_aging/Y20O20/chipseq/indexes", GENOME)
+    INDEXES = os.path.join("/scratch/artyomov_lab_aging/Y20O20/chipseq/indexes",
+                           GENOME)
     CHROM_SIZES = os.path.join(INDEXES, GENOME + ".chrom.sizes")
 
     # Data table
@@ -64,28 +66,11 @@ def cli(out, data):
 
     # Download SRA data:
     # 'rsync' here skips file if it already exist
-    for gsmid in gsm_to_process:
-
-        sra_dir = os.path.join(out, gsmid, "sra")
-        os.makedirs(sra_dir, exist_ok=True)
-
-        print("Downloading data to {} ...".format(sra_dir))
-        srxid = gsm2srx[gsmid]
-        url = "rsync://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/" \
-              "sra/SRX/{}/{}".format(srxid[0:6], srxid)
-
-        # Options:
-        #   -a, --archive            archive mode; equals -rlptgoD (no -H,-A,-X)
-        #   -z, --compress           compress file data during the transfer
-        #   -v, --verbose            increase verbosity
-        #   -i,  --itemize-changes   output a change-summary for all updates
-        #   -t, --times              preserve modification times
-        #   —partial (or -P = —progress —partial): enable partial transmission,
-        #                            but not for local fs
-        #   -r, --recursive          recurse into directories
-        #   -h, --human-readable     output numbers in a human-readable format
-        run("rsync -azvvit --partial-dir=.rsync-partial --human-readable"
-            " --progress {} {}".format(url, sra_dir))
+    print("Downloading data...")
+    run_bash("geo_rsync.sh",
+             *itertools.chain(*[(gsm2srx[gsmid],
+                                 os.path.join(out, gsmid, "sra"))
+                                for gsmid in gsm_to_process]))
 
     # Fastq-dump SRA data:
     run_bash("fastq_dump.sh", *data_dirs)
@@ -102,52 +87,53 @@ def cli(out, data):
     if len(data_dirs) > 1:
         run("multiqc", "-f", "-o", WORK_DIR, " ".join(data_dirs))
 
-    # # Batch Bowtie with trim 5 first base pairs
-    # run_bash("index_bowtie.sh", GENOME, INDEXES)
-    # run_bash("bowtie.sh", WORK_DIR, GENOME, INDEXES, "5")
-    # WORK_DIR = move_forward(WORK_DIR, WORK_DIR + "_bams", ["*.bam", "*bowtie*.log"])
-    # # multiqc is able to process Bowtie report
-    # subprocess.run("multiqc " + WORK_DIR, shell=True)
-    # # Create summary
-    # process_bowtie_logs(WORK_DIR)
-    #
-    # # Process insert size of BAM visualization
-    # run_bash("fragments.sh", WORK_DIR)
-    #
-    # # Batch BigWig visualization
-    # run_bash("bigwig.sh", WORK_DIR, CHROM_SIZES)
-    # move_forward(WORK_DIR, WORK_DIR + "_bws", ["*.bw", "*.bdg", "*bw.log"], copy_only=True)
-    #
-    # # Batch RPKM visualization
-    # run_bash("rpkm.sh", WORK_DIR)
-    # move_forward(WORK_DIR, WORK_DIR + "_rpkms", ["*.bw", "*rpkm.log"], copy_only=True)
-    #
-    # # Remove duplicates
-    # run_bash("remove_duplicates.sh", WORK_DIR)
-    # move_forward(WORK_DIR, WORK_DIR + "_unique", ["*_unique*", "*_metrics.txt", "*duplicates.log"], copy_only=True)
-    #
-    # # Batch subsampling to 15mln reads
-    # # READS = 15
-    # # run_bash("subsample.sh", WORK_DIR, str(READS))
-    # # WORK_DIR = move_forward(WORK_DIR, WORK_DIR + "_{}mln".format(READS), ["*{}*".format(READS)])
-    # #
-    # # # Batch BigWig visualization
-    # # run_bash("bigwig.sh", WORK_DIR, CHROM_SIZES)
-    # # move_forward(WORK_DIR, WORK_DIR + "_bws", ["*.bw", "*.bdg", "*bw.log"], copy_only=True)
-    #
-    # ########################
-    # # Peak calling section #
-    # ########################
-    #
-    # # MACS2 Broad peak calling (https://github.com/taoliu/MACS) Q=0.1 in example
-    # folder = run_macs2(WORK_DIR, GENOME, CHROM_SIZES, 'broad_0.1', '--broad', '--broad-cutoff', 0.1)
-    # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.05'), 0.1, 0.05, WORK_DIR)
-    # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.01'), 0.1, 0.01, WORK_DIR)
-    #
-    # # MACS2 Regular peak calling (https://github.com/taoliu/MACS) Q=0.01 in example
-    # folder = run_macs2(WORK_DIR, GENOME, CHROM_SIZES, 'q0.1', '-q', 0.1)
-    # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.05'), 0.1, 0.05, WORK_DIR)
-    # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.01'), 0.1, 0.01, WORK_DIR)
+        # # Batch Bowtie with trim 5 first base pairs
+        # run_bash("index_bowtie.sh", GENOME, INDEXES)
+        # run_bash("bowtie.sh", WORK_DIR, GENOME, INDEXES, "5")
+        # WORK_DIR = move_forward(WORK_DIR, WORK_DIR + "_bams", ["*.bam", "*bowtie*.log"])
+        # # multiqc is able to process Bowtie report
+        # subprocess.run("multiqc " + WORK_DIR, shell=True)
+        # # Create summary
+        # process_bowtie_logs(WORK_DIR)
+        #
+        # # Process insert size of BAM visualization
+        # run_bash("fragments.sh", WORK_DIR)
+        #
+        # # Batch BigWig visualization
+        # run_bash("bigwig.sh", WORK_DIR, CHROM_SIZES)
+        # move_forward(WORK_DIR, WORK_DIR + "_bws", ["*.bw", "*.bdg", "*bw.log"], copy_only=True)
+        #
+        # # Batch RPKM visualization
+        # run_bash("rpkm.sh", WORK_DIR)
+        # move_forward(WORK_DIR, WORK_DIR + "_rpkms", ["*.bw", "*rpkm.log"], copy_only=True)
+        #
+        # # Remove duplicates
+        # run_bash("remove_duplicates.sh", WORK_DIR)
+        # move_forward(WORK_DIR, WORK_DIR + "_unique", ["*_unique*", "*_metrics.txt", "*duplicates.log"], copy_only=True)
+        #
+        # # Batch subsampling to 15mln reads
+        # # READS = 15
+        # # run_bash("subsample.sh", WORK_DIR, str(READS))
+        # # WORK_DIR = move_forward(WORK_DIR, WORK_DIR + "_{}mln".format(READS), ["*{}*".format(READS)])
+        # #
+        # # # Batch BigWig visualization
+        # # run_bash("bigwig.sh", WORK_DIR, CHROM_SIZES)
+        # # move_forward(WORK_DIR, WORK_DIR + "_bws", ["*.bw", "*.bdg", "*bw.log"], copy_only=True)
+        #
+        # ########################
+        # # Peak calling section #
+        # ########################
+        #
+        # # MACS2 Broad peak calling (https://github.com/taoliu/MACS) Q=0.1 in example
+        # folder = run_macs2(WORK_DIR, GENOME, CHROM_SIZES, 'broad_0.1', '--broad', '--broad-cutoff', 0.1)
+        # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.05'), 0.1, 0.05, WORK_DIR)
+        # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.01'), 0.1, 0.01, WORK_DIR)
+        #
+        # # MACS2 Regular peak calling (https://github.com/taoliu/MACS) Q=0.01 in example
+        # folder = run_macs2(WORK_DIR, GENOME, CHROM_SIZES, 'q0.1', '-q', 0.1)
+        # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.05'), 0.1, 0.05, WORK_DIR)
+        # run_bash("../bed/macs2_filter_fdr.sh", folder, folder.replace('0.1', '0.01'), 0.1, 0.01, WORK_DIR)
+
 
 if __name__ == '__main__':
     cli()
---
+++ scripts/geo_rsync.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+HELP=NO
+############################################################################################################
+if [[ $# -eq 0 ]]; then
+  HELP=YES
+fi
+
+PAIRS=""
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case ${key} in
+        -h|--help)
+        HELP=YES
+        shift
+        ;;
+
+        *) # Last arg as session name
+        WORK_DIR="$2"
+        PAIRS="${PAIRS};${key}=${WORK_DIR}"
+        shift
+        shift
+        ;;
+    esac
+done
+
+if [ ${HELP} == "YES" ]; then
+    echo "For each given SRX_ID_i and WORK_DIR_i pair this script downloads all *.sra from
+rsync://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRXnnn/SRXnnnnn
+to given directory.
+
+Usage:
+  geo_rsync.sh SRX_ID_1 WORK_DIR_1 [SRX_ID_2 WORK_DIR_2 ..]"
+    exit 0
+fi
+
+IFS=';' list=(${PAIRS})
+for item in "${list[@]}"; do
+    if [[ ! -z ${item} ]]; then
+        WORK_DIR="${item#*=}"
+        SRXID="${item%=*}"
+
+        URL="rsync://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/${SRXID:0:6}/${SRXID}"
+        echo "Downloading: ${URL}"
+        # Options:
+        #   -a, --archive            archive mode; equals -rlptgoD (no -H,-A,-X)
+        #   -z, --compress           compress file data during the transfer
+        #   -v, --verbose            increase verbosity
+        #   -i,  --itemize-changes   output a change-summary for all updates
+        #   -t, --times              preserve modification times
+        #   —partial (or -P = —progress —partial): enable partial transmission,
+        #                            but not for local fs
+        #   -r, --recursive          recurse into directories
+        #   -h, --human-readable     output numbers in a human-readable format
+        rsync -azvvit --partial-dir=.rsync-partial --human-readable --progress ${URL} ${WORK_DIR}
+    fi
+done
