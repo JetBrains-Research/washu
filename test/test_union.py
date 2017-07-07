29ce28a172d1c0d8442e4bff6fe220331ec59752
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-07-07 15:57:28 +0300

    Fix test_union.py after commit: 26ff2e1f8c7d59ac4b1869aeec547955eb449d6e

--- test/test_union.py
+++ test/test_union.py
@@ -18,11 +18,11 @@ class UnionTest(unittest.TestCase):
     def test_union(self):
         ps = subprocess.Popen(['bash', UNION_SH, TEST_DATA + '/A.bed', TEST_DATA + '/B.bed', TEST_DATA + '/C.bed'],
                               stdout=subprocess.PIPE)
-        self.assertEqual("""chr1	0	100	1|3|3
-chr1	150	500	2|1|3|1|3
-chr1	600	750	1|2
-chr1	800	850	3
-""", ps.communicate()[0].decode("utf-8"))
+        self.assertEqual("""chr1	0	100	1_A.bed|3_C.bed
+chr1	150	500	1_A.bed|2_B.bed|3_C.bed
+chr1	600	750	1_A.bed|2_B.bed
+chr1	800	850	3_C.bed
+""", ps.communicate()[0].decode("utf-8").replace(TEST_DATA + '/', ''))
 
 if __name__ == '__main__':
     unittest.main()
