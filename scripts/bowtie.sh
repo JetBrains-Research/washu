0ad53b4207561909ad67c41c1b8eb2aa173db96f
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-07-27 21:21:26 +0300

    bowtie.sh support multiple input workdirs

--- scripts/bowtie.sh
+++ scripts/bowtie.sh
@@ -7,65 +7,75 @@ if [ -f "$(dirname $0)/util.sh" ]; then
 fi
 
 if [ $# -lt 4 ]; then
-    echo "Need 4 parameters! <GENOME> <INDEXES> <TRIM5> <WORK_DIR>"
+    echo "Need 4 parameters! <GENOME> <INDEXES> <TRIM5> <WORK_DIR> [<WORK_DIR>]"
     exit 1
 fi
+
 GENOME=$1
 INDEXES=$2
 TRIM5=$3
-WORK_DIR=$4
+WORK_DIRS=${@:4}
 
-echo "Batch Bowtie: ${GENOME} ${INDEXES} ${TRIM5} ${WORK_DIR}"
-cd ${WORK_DIR}
+echo "Batch Bowtie: ${GENOME} ${INDEXES} ${TRIM5} ${WORK_DIRS}"
 
-PROCESSED=""
 TASKS=""
+PROCESSED=""
 
-# Fails with large indexes, create soft link to indexes in working directory as a workaround
-# export BOWTIE_INDEXES=${INDEXES}
-if [ ! -d "${WORK_DIR}/indexes" ]; then
-    ln -s ${INDEXES} ${WORK_DIR}/indexes
-fi
+for WORK_DIR in ${WORK_DIRS}; do :
+    WORK_DIR_NAME=${WORK_DIR##*/}
+    cd ${WORK_DIR}
 
-# Bowtie fails with large indexes, explicitly set
-if [ -f indexes/${GENOME}.1.ebwtl ]; then
-    INDEX_ARG="--large-index"
-else
-    INDEX_ARG=""
-fi
-
-for FILE in $(find . -name '*.f*q' | sed 's#./##g' | sort)
-do :
-    if $(echo "${PROCESSED[@]}"  | fgrep -q "${FILE}");
-    then
-        echo "$FILE was already processed"
-        continue
+    # Fails with large indexes, create soft link to indexes in working directory as a workaround
+    # export BOWTIE_INDEXES=${INDEXES}
+    if [ ! -d "${WORK_DIR}/indexes" ]; then
+        ln -s ${INDEXES} ${WORK_DIR}/indexes
     fi
 
-    # Assumption: the only difference between paired-end read files is _1 and _2
-    FILE_PAIRED=""
-    if $(echo "${FILE}"  | fgrep -q "_1");
-    then
-        PREFIX=${FILE%%_1.*}
-        SUFFIX=${FILE##*_1}
-        FILE_PAIRED="${PREFIX}_2${SUFFIX}"
-    fi
-
-    # Setup correct name
-    if [ -f "${FILE_PAIRED}" ]; then
-        echo "PAIRED END reads detected: $FILE and $FILE_PAIRED"
-        # Mark it as already processed
-        PROCESSED="${PROCESSED} ${FILE_PAIRED}"
-        NAME="${PREFIX}${SUFFIX}"
+    # Bowtie fails with large indexes, explicitly set
+    if [ -f indexes/${GENOME}.1.ebwtl ]; then
+        INDEX_ARG="--large-index"
     else
-        NAME=${FILE%%.f*q}
+        INDEX_ARG=""
     fi
-    ID=${NAME}_${GENOME}
 
-    # Submit task
-    QSUB_ID=$(qsub << ENDINPUT
+    for FILE in $(find . -name '*.f*q' | sed 's#\./##g' | sort)
+    do :
+        if $(echo "${PROCESSED[@]}"  | fgrep -q "${FILE}");
+        then
+            echo "$FILE was already processed"
+            continue
+        fi
+
+        # Assumption: the only difference between paired-end read files is _1 and _2
+        FILE_PAIRED=""
+        if $(echo "${FILE}"  | fgrep -q "_1");
+        then
+            PREFIX=${FILE%%_1.*}
+            SUFFIX=${FILE##*_1}
+            FILE_PAIRED="${PREFIX}_2${SUFFIX}"
+        fi
+
+        # Setup correct name
+        if [ -f "${FILE_PAIRED}" ]; then
+            echo "PAIRED END reads detected: $FILE and $FILE_PAIRED"
+            # Mark it as already processed
+            PROCESSED="${PROCESSED} ${FILE_PAIRED}"
+            NAME="${PREFIX}${SUFFIX}"
+        else
+            NAME=${FILE%%.f*q}
+        fi
+        NAME="${NAME##*/}" # if relative path - trim folders
+        ID=${NAME}_${GENOME}
+
+        echo "ID=${ID}"
+        echo "wd=${WORK_DIR}"
+        echo "fp=${FILE_PAIRED}"
+        echo "file=${FILE}"
+
+        # Submit task
+        QSUB_ID=$(qsub << ENDINPUT
 #!/bin/sh
-#PBS -N bowtie_${GENOME}_${NAME}
+#PBS -N bowtie_${GENOME}_${WORK_DIR_NAME}_${NAME}
 #PBS -l nodes=1:ppn=4,walltime=24:00:00,vmem=32gb
 #PBS -j oe
 #PBS -o ${WORK_DIR}/${NAME}_bowtie_${GENOME}.log
@@ -100,19 +110,22 @@ rm ${ID}.sam ${ID}_not_sorted.bam
 
 ENDINPUT
 )
-    if [ -f "${FILE_PAIRED}" ]; then
-        echo "FILE: ${FILE} PAIRED ${FILE_PAIRED}; TASK: ${QSUB_ID}"
-    else
-        echo "FILE: ${FILE}; TASK: ${QSUB_ID}"
-    fi
-    TASKS="$TASKS $QSUB_ID"
+        if [ -f "${FILE_PAIRED}" ]; then
+            echo "FILE: ${WORK_DIR_NAME}:${FILE} PAIRED ${FILE_PAIRED}; TASK: ${QSUB_ID}"
+        else
+            echo "FILE: ${WORK_DIR_NAME}:${FILE}; TASK: ${QSUB_ID}"
+        fi
+        TASKS="$TASKS $QSUB_ID"
+    done
 done
 wait_complete ${TASKS}
 check_logs
 
 # Cleanup indexes soft link
-if [ -d "${WORK_DIR}/indexes" ]; then
-    rm ${WORK_DIR}/indexes
-fi
+for WORK_DIR in ${WORK_DIRS}; do :
+    if [ -d "${WORK_DIR}/indexes" ]; then
+        rm ${WORK_DIR}/indexes
+    fi
+done
 
-echo "Done. Batch Bowtie: ${GENOME} ${INDEXES} ${TRIM5} ${WORK_DIR}"
+echo "Done. Batch Bowtie: ${GENOME} ${INDEXES} ${TRIM5} ${WORK_DIRS}"
