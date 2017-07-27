d7d395aac34b4c76e9750eedee7d6897670829b4
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2017-07-27 13:22:36 +0200

    Add input support. Add CHROM_SIZES parameter.

--- scripts/zinbra.sh
+++ scripts/zinbra.sh
@@ -6,23 +6,26 @@ if [ -f "$(dirname $0)/util.sh" ]; then
     source "$(dirname $0)/util.sh"
 fi
 
-if [ $# -lt 4 ]; then
-    echo "Need 5 parameters! <ZINBRA_JAR_PATH> <WORK_DIR> <GENOME> <FOLDER> <Q>"
+if [ $# -lt 5 ]; then
+    echo "Need 5 parameters! <ZINBRA_JAR_PATH> <WORK_DIR> <GENOME> <CHROM_SIZES> <Q>"
     exit 1
 fi
 
 ZINBRA_JAR_PATH=$1
 WORK_DIR=$2
 GENOME=$3
-FOLDER=$4
+CHROM_SIZES=$4
 Q=$5
 
-echo "Batch zinbra: ${ZINBRA_JAR_PATH} ${WORK_DIR} ${GENOME} ${FOLDER} ${Q}"
+echo "Batch zinbra: ${ZINBRA_JAR_PATH} ${WORK_DIR} ${GENOME} ${CHROM_SIZES} ${Q}"
 cd ${WORK_DIR}
 
 TASKS=""
 for FILE in $(find . -name '*.bam' | sed 's#./##g' | grep -v 'input')
 do :
+    INPUT=$(python $(dirname $0)/util.py find_input ${WORK_DIR}/${FILE})
+    echo "${FILE}: control file: ${INPUT}"
+
     NAME=${FILE%%.bam} # file name without extension
     ID=${NAME}_${GENOME}_${Q}
 
@@ -41,8 +44,19 @@ module load java
 # PROBLEM: vmem is much bigger, however we face with the problem with bigger values:
 # There is insufficient memory for the Java Runtime Environment to continue.
 export _JAVA_OPTIONS="-Xmx30g"
-java -cp ${ZINBRA_JAR_PATH} org.jetbrains.bio.zinbra.ZinbraCLA analyze -c ${FILE} \
-    --chrom.sizes ${FOLDER}/${GENOME}.chrom.sizes --fdr ${Q} --bed ${ID}_peaks.bed
+
+if [ -f "${INPUT}" ]; then
+    echo "${FILE}: control file found: ${INPUT}"
+    java -cp ${ZINBRA_JAR_PATH} org.jetbrains.bio.zinbra.ZinbraCLA analyze -t ${FILE} -c ${FILE} \
+        --chrom.sizes ${CHROM_SIZES} --fdr ${Q} --bed ${ID}_peaks.bed
+
+else
+    echo "${FILE}: no control file"
+    java -cp ${ZINBRA_JAR_PATH} org.jetbrains.bio.zinbra.ZinbraCLA analyze -t ${FILE} \
+        --chrom.sizes ${CHROM_SIZES} --fdr ${Q} --bed ${ID}_peaks.bed
+fi
+
+
 ENDINPUT
 )
     echo "FILE: ${FILE}; TASK: ${QSUB_ID}"
@@ -51,4 +65,4 @@ done
 wait_complete ${TASKS}
 check_logs
 
-echo "Done. Batch zinbra: ${ZINBRA_JAR_PATH} ${WORK_DIR} ${GENOME} ${FOLDER} ${Q}"
+echo "Done. Batch zinbra: ${ZINBRA_JAR_PATH} ${WORK_DIR} ${GENOME} ${CHROM_SIZES} ${Q}"
