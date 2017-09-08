b8b0337521405df839463a7ca1ad7862576386d2
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-09-09 01:29:05 +0300

    test converted to pytest

--- test/test_chip_seq_pipeline.py
+++ test/test_chip_seq_pipeline.py
@@ -1,12 +1,15 @@
-import unittest
 from subprocess import call
 import os
 import shutil
 import glob
 
+import pytest
+from test.fixtures import test_data, tmp_dir
+
 
 def run_pipeline():
     os.chdir(os.path.expanduser("~"))
+    # os.chdir(tmp_dir)
 
     call(["tar", "xfz", "chip-seq-pipeline-test-data.tar.gz"])
 
@@ -25,48 +28,50 @@ def run_pipeline():
     call(["python", "/washu/pipeline_chipseq.py", "/root/H3K4me3"])
 
 
-class ChipSeqPipeline(unittest.TestCase):
-    def check_files_not_empty(self, pattern, expected_files_number=None):
-        files = glob.glob(pattern)
-        self.assertEqual(expected_files_number, len(files),
-                         "Expected {} files for pattern '{}', but {} found.".format(expected_files_number, pattern,
-                                                                                    len(files)))
+def check_files_not_empty(pattern, expected_files_number=None):
+    files = glob.glob(pattern)
+    
+    print("Expected {} files for pattern '{}'".format(expected_files_number, 
+                                                      pattern))
+    assert len(files) == expected_files_number
 
-        for f in files:
-            self.assertNotEqual(0, os.path.getsize(f), "File {} is empty.".format(f))
+    for f in files:
+        assert os.path.getsize(f) == 0
 
-    def check_files(self):
-        os.chdir(os.path.expanduser("~"))
-        self.check_files_not_empty("./H3K4me3_bams/*.bam", 5)
-        self.check_files_not_empty("./H3K4me3_bams/*.bai", 5)
 
-        self.check_files_not_empty("./H3K4me3_bams_macs2_broad_0.01/*.broadPeak", 4)
-        self.check_files_not_empty("./H3K4me3_bams_macs2_broad_0.01/*.broadPeak_rip.csv", 4)
+def check_files():
+    os.chdir(os.path.expanduser("~"))
+    check_files_not_empty("./H3K4me3_bams/*.bam", 5)
+    check_files_not_empty("./H3K4me3_bams/*.bai", 5)
 
-        self.check_files_not_empty("./H3K4me3_bams_macs2_broad_0.05/*.broadPeak", 4)
-        self.check_files_not_empty("./H3K4me3_bams_macs2_broad_0.05/*.broadPeak_rip.csv", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_broad_0.01/*.broadPeak", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_broad_0.01/*.broadPeak_rip.csv", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_macs2_broad_0.1/*.broadPeak", 4)
-        self.check_files_not_empty("./H3K4me3_bams_macs2_broad_0.1/*.broadPeak_rip.csv", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_broad_0.05/*.broadPeak", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_broad_0.05/*.broadPeak_rip.csv", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_macs2_q0.01/*.narrowPeak", 4)
-        self.check_files_not_empty("./H3K4me3_bams_macs2_q0.01/*.narrowPeak_rip.csv", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_broad_0.1/*.broadPeak", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_broad_0.1/*.broadPeak_rip.csv", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_macs2_q0.05/*.narrowPeak", 4)
-        self.check_files_not_empty("./H3K4me3_bams_macs2_q0.05/*.narrowPeak_rip.csv", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_q0.01/*.narrowPeak", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_q0.01/*.narrowPeak_rip.csv", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_macs2_q0.1/*.narrowPeak", 4)
-        self.check_files_not_empty("./H3K4me3_bams_macs2_q0.1/*.narrowPeak_rip.csv", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_q0.05/*.narrowPeak", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_q0.05/*.narrowPeak_rip.csv", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_rpkms/*.bw", 5)
+    check_files_not_empty("./H3K4me3_bams_macs2_q0.1/*.narrowPeak", 4)
+    check_files_not_empty("./H3K4me3_bams_macs2_q0.1/*.narrowPeak_rip.csv", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_rseg/*_domains.bed", 4)
+    check_files_not_empty("./H3K4me3_bams_rpkms/*.bw", 5)
 
-        self.check_files_not_empty("./H3K4me3_bams_sicer_0.01/*-island.bed", 4)
-        self.check_files_not_empty("./H3K4me3_bams_sicer_0.01/*-island.bed_rip.csv", 4)
+    check_files_not_empty("./H3K4me3_bams_rseg/*_domains.bed", 4)
 
-        self.check_files_not_empty("./H3K4me3_bams_unique/*.bam", 5)
+    check_files_not_empty("./H3K4me3_bams_sicer_0.01/*-island.bed", 4)
+    check_files_not_empty("./H3K4me3_bams_sicer_0.01/*-island.bed_rip.csv", 4)
 
-    def test_pipeline(self):
-        run_pipeline()
-        self.check_files()
+    check_files_not_empty("./H3K4me3_bams_unique/*.bam", 5)
+
+
+def test_pipeline():
+    run_pipeline()
+    check_files()
