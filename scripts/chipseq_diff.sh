d9579f1c1519a2b21fa517c06b47dd38c4f5a5b3
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2017-09-25 14:26:50 +0200

    Fix scripts folder

--- scripts/chipseq_diff.sh
+++ scripts/chipseq_diff.sh
@@ -5,9 +5,9 @@
 # Example of ChIP-Seq differential analysis:
 # cd /scratch/artyomov_lab_aging/Y20O20/chipseq/processed/k4me3
 # mkdir -p k4me3_diff
-# bash ~/work/washu/analysis/diff_config.sh k4me3_20vs20_bams k4me3_20vs20_bams_macs2_broad_0.1 > k4me3_diff/k4me3.csv
+# bash ~/work/washu/scripts/diff_config.sh k4me3_20vs20_bams k4me3_20vs20_bams_macs2_broad_0.1 > k4me3_diff/k4me3.csv
 # cd k4me3_diff
-# bash ~/work/washu/analysis/chipseq_diff.sh k4me3 /scratch/artyomov_lab_aging/Y20O20/chipseq/indexes/hg19/hg19.chrom.sizes k4me3.csv
+# bash ~/work/washu/scripts/chipseq_diff.sh k4me3 /scratch/artyomov_lab_aging/Y20O20/chipseq/indexes/hg19/hg19.chrom.sizes k4me3.csv
 
 # Check tools
 which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
