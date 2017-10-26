544e31afbf8b04fede2260e32bdd7fa1c126c6ee
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-10-26 18:26:51 +0300

    loci of interest: heatmap plotting scripted

--- reports/bed_metrics.py
+++ reports/bed_metrics.py
@@ -1,15 +1,19 @@
 import os
 from multiprocessing import Pool, TimeoutError
-from typing import List
+from typing import List, Tuple
+from collections import defaultdict
 
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 from scripts.util import run
 
 
-def _run_metric_intersection(a, b, *args):
+def _run_metric_intersection(a, b, *args, **kw):
     """
     Metric intersection (#1):
         (wc -l $a) / (bedtools intersect -a $a -b $b -wa | uniq | wc -l)
@@ -29,7 +33,7 @@ def _run_metric_intersection(a, b, *args):
     return (n_intersecting / n_total, *args)
 
 
-def _run_metric_jaccard(a, b, *args):
+def _run_metric_jaccard(a, b, *args, **kw):
     """
     :param id: Id for parallel execution
     :param a: A.bed
@@ -37,15 +41,28 @@ def _run_metric_jaccard(a, b, *args):
     :return: Jaccard index by `~/work/washu/bed/jaccard.sh $a $b`
     """
     script = os.path.expanduser("~/work/washu/bed/jaccard.sh")
-    output = run([["bash", script, str(a), str(b)]])
+    cmdline = ["bash", script, str(a), str(b)]
+    if kw.get("sorted", False):
+        cmdline.append("-s")
+    if kw.get("merged", False):
+        cmdline.append("-m")
+    output = run([cmdline])
     stdout = output[0].decode().strip()
     return (float(stdout), *args)
 
 
-def intersections_table(a_paths: List[Path], b_paths: List[Path],
-                        jaccard=False,
-                        threads=4, timeout_hours=10):
-
+def bed_metric_table(a_paths: List[Path], b_paths: List[Path],
+                     jaccard=False,
+                     threads=4, timeout_hours=10, **kw):
+    """
+    :param a_paths: First paths set
+    :param b_paths:  Second paths set
+    :param jaccard: If True use Jaccard metric, else Intersection metric
+    :param threads: Threads number for parallel execution
+    :param timeout_hours: Execution timeout in hours
+    :param kw: Additional args, e.g.: "sorted" or "merged" for jaccard
+    :return: Dataframe selected metric for A x B
+    """
     path_pairs = []
     for i, a in enumerate(a_paths, 0):
         for j, b in enumerate(b_paths, 0):
@@ -54,9 +71,8 @@ def intersections_table(a_paths: List[Path], b_paths: List[Path],
     inner_metric = _run_metric_jaccard if jaccard else _run_metric_intersection
 
     pool = Pool(processes=threads)
-    multiple_results = [pool.apply_async(inner_metric,
-                                         (a, b, ij)) for a, b, ij in
-                        path_pairs]
+    multiple_results = [pool.apply_async(inner_metric, (a, b, ij), kw)
+                        for a, b, ij in path_pairs]
     values = [res.get(timeout=3600 * timeout_hours) for res in
               multiple_results]
 
@@ -67,11 +83,106 @@ def intersections_table(a_paths: List[Path], b_paths: List[Path],
     df = pd.DataFrame(x,
                       index=[f.name for f in a_paths],
                       columns=[f.name for f in b_paths])
-    # for i, a in enumerate(a_paths, 0):
-    #     output = run((["cat", str(a)], ["wc", "-l"],))
-    #     x[i, 0] = int(output[0].decode().strip())
-    #
-    # df = pd.DataFrame(x,
-    #                   index=[f.name for f in a_paths],
-    #                   columns=["total"] + [f.name for f in b_paths])
     return df
+
+
+# def load_intersection_table(beds, loci, path_to_bt, result_path, *kw):
+#     if result_path.exists():
+#         df = pd.DataFrame.from_csv(result_path)
+#         print("Loaded: ", result_path)
+#     else:
+#         print("Calculating: ", result_path)
+#         df = intersections_table(beds, loci, *kw)
+#         result_path.parent.mkdir(parents=True, exist_ok=True)
+#         df.to_csv(str(result_path))
+#         print("  Saved: ", result_path)
+#
+#     return df
+
+
+def heatmap_donor_color_fun():
+    def inner(label) -> Tuple[Tuple[str, str]]:
+        chunks = [ch.lower() for ch in label.split("_")]
+
+        for chunk in chunks:
+            ch = chunk.lower()
+            if ch.startswith("od"):
+                return (("age", "b"),)
+            elif ch.startswith("yd"):
+                return (("age", "r"),)
+
+        return (("age", "gray"),)
+
+    return inner
+
+
+def plot_metric_heatmap(title, df, figsize=(10, 10),
+                        save_to=None,
+                        vmin=0, vmax=1,
+                        col_color_fun=None, row_color_fun=None,
+                        col_label_fun=None, row_label_fun=None,
+                        col_cluster=False, row_cluster=False):
+
+    """
+    :param title: Plot title
+    :param df: Dataframe with metric value
+    :param figsize: Plot size
+    :param save_to: Destination: 1) string path 2) Pdf obj 3) None to
+           plot on the screen
+    :param vmin: Heatmap min value (use None to infer from data)
+    :param vmax: Heatmap max value (use None to infer from data)
+    :param col_color_fun: Function which highlights cols
+    :param row_color_fun: Function which highlights rows
+    :param col_label_fun: Function which modifies cols names
+    :param row_label_fun: Function which modifies rows names
+    :param col_cluster: see seaborn.clustermap(..) details
+    :param row_cluster: see seaborn.clustermap(..) details
+    """
+    if col_label_fun or row_label_fun:
+        df = df.copy()
+
+        if col_label_fun:
+            df.columns = [col_label_fun(s) for s in df.columns]
+
+        if row_label_fun:
+            df.index = [row_label_fun(s) for s in df.index]
+
+    def as_colors_df(color_fun, items):
+        data = defaultdict(list)
+        for item in items:
+            for k, v in color_fun(item):
+                data[k].append(v)
+        assert len({len(colors) for col, colors in data.items()}) == 1,\
+            "All color list should be equal size"
+
+        df = pd.DataFrame.from_dict(data)
+        df.index = items
+        return df
+
+    c_colors = None if not col_color_fun else as_colors_df(col_color_fun,
+                                                           df.columns)
+    r_colors = None if not row_color_fun else as_colors_df(row_color_fun,
+                                                           df.index)
+
+    # TODO: for jaccard use dist function? matrix could be not square here
+    g = sns.clustermap(
+        df, figsize=figsize, cmap="rainbow",
+        col_cluster=col_cluster, row_cluster=row_cluster, metric="chebyshev",
+        col_colors=c_colors, row_colors=r_colors,
+        vmin=vmin, vmax=vmax,
+        robust=True,  # robust=True: ignore color outliers
+    )
+
+    plt.setp(g.ax_heatmap.get_yticklabels(), rotation=0)
+
+    plt.title(title)
+    if save_to is None:
+        plt.show()
+    elif isinstance(save_to, str):
+        plt.savefig(str(save_to))
+        plt.close()
+    elif isinstance(save_to, PdfPages):
+        save_to.savefig()
+        plt.close()
+    else:
+        raise ValueError("Unsupported value type: {}".format(type(save_to)))
--- test/test_bed_metrics.py
+++ test/test_bed_metrics.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import pandas as pd
 import pytest
-from test.fixtures import test_data, bedtrace_cleanup
+from test.fixtures import test_data, tmp_dir
 
-from reports.bed_metrics import intersections_table
+from reports.bed_metrics import bed_metric_table, heatmap_donor_color_fun, \
+    plot_metric_heatmap
 
 
 @pytest.mark.parametrize("jaccard,fname,swap_ab", [
@@ -12,14 +13,108 @@ from reports.bed_metrics import intersections_table
     (True, "metric_j.csv", False),
     (True, "metric_j_ba.csv", True)
 ])
-def test_intersections_table(test_data, jaccard, fname, swap_ab):
+def test_bed_metric_table(test_data, jaccard, fname, swap_ab):
     a_paths \
         = [Path(test_data("metrics/c{}.bed".format(i))) for i in range(1, 4)]
     b_paths = [Path(test_data("metrics/a.bed")), *a_paths]
 
-    df = intersections_table(a_paths if not swap_ab else b_paths,
-                             b_paths if not swap_ab else a_paths,
-                             jaccard=jaccard)
+    df = bed_metric_table(a_paths if not swap_ab else b_paths,
+                          b_paths if not swap_ab else a_paths,
+                          jaccard=jaccard)
     expected = pd.DataFrame.from_csv(test_data("metrics/{}".format(fname)))
 
     assert str(expected) == str(df)
+
+
+@pytest.mark.parametrize("name,color", [
+    ("od", "b"),
+    ("od1", "b"),
+    ("od_od1", "b"),
+    ("ods", "b"),
+    ("foo_od2_boo", "b"),
+    ("foyd_od", "b"),
+    ("OD1", "b"),
+    ("fo_OD2", "b"),
+    ("fo_OD_YD1", "b"),
+
+    ("yd", "r"),
+    ("yd1", "r"),
+    ("yd_yd1", "r"),
+    ("yds", "r"),
+    ("foo_yd2_boo", "r"),
+    ("food_yd", "r"),
+    ("YD1", "r"),
+    ("fo_YD2", "r"),
+    ("fo_YD_OD1", "r"),
+
+    ("fo.OD2", "gray"),
+    ("fo.YD2", "gray"),
+])
+def test_heatmap_donor_color_fun(name, color):
+    res = heatmap_donor_color_fun()(name)
+    assert 1 == len(res)
+    assert "age" == res[0][0]
+    assert color == res[0][1]
+
+
+def assert_image(expected_path, actual_path):
+    with open(expected_path, "rb") as ef:
+        with open(actual_path, "rb") as af:
+            expected = ef.read()
+            actual = af.read()
+            assert len(expected) == len(actual)
+            assert expected == actual
+
+
+def test_plot_metric_heatmap(tmp_dir, test_data):
+    df = pd.DataFrame.from_csv(test_data("metrics/metric1.csv"))
+    # plot_metric_heatmap("My title", df, save_to=test_data("metrics/img1.png"))
+
+    result = tmp_dir + "foo.png"
+    plot_metric_heatmap("My title", df, save_to=result)
+
+    assert_image(test_data("metrics/img1.png"), result)
+
+
+@pytest.mark.parametrize("fname,col,row", [
+    ("img1.png", False, False),
+    ("img1_l-c.png", True, False),
+    ("img1_l-r.png", False, True),
+    ("img1_l-cr.png", True, True),
+])
+def test_plot_metric_heatmap_label_fun(tmp_dir, test_data, fname, col, row):
+    df = pd.DataFrame.from_csv(test_data("metrics/metric1.csv"))
+
+    expected = test_data("metrics/" + fname)
+    result = tmp_dir + "/foo.png"
+
+    col_label_fun = None if not col else lambda x: "[c]" + x
+    row_label_fun = None if not row else lambda x: "[r]" + x
+    plot_metric_heatmap("My title", df, save_to=result,
+                        col_label_fun=col_label_fun,
+                        row_label_fun=row_label_fun)
+
+    assert_image(expected, result)
+
+
+@pytest.mark.parametrize("fname,col,row", [
+    ("img1_c.png", False, False),
+    ("img1_c-c.png", True, False),
+    ("img1_c-r.png", False, True),
+    ("img1_c-rl.png", True, True),
+])
+def test_plot_metric_heatmap_col_fun(tmp_dir, test_data, fname, col, row):
+    df = pd.DataFrame.from_csv(test_data("metrics/metric1.csv"))
+    df.columns = ["ODS", "boo", "YDS", "YD20"]
+    df.index = ["OD1", "foo", "boo_YD1"]
+
+    expected = test_data("metrics/" + fname)
+    result = tmp_dir + "/foo.png"
+
+    col_col_fun = None if not col else heatmap_donor_color_fun()
+    row_col_fun = None if not row else heatmap_donor_color_fun()
+    plot_metric_heatmap("My title", df, save_to=result,
+                        col_color_fun=col_col_fun,
+                        row_color_fun=row_col_fun)
+
+    assert_image(expected, result)
