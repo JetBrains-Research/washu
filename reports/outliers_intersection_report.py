b1d9515ddba79ea378671fe2bf79bc9cdac24f7c
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-10-27 12:30:35 +0300

    loading fixed

--- reports/outliers_intersection_report.py
+++ reports/outliers_intersection_report.py
@@ -10,15 +10,15 @@ sys.path.insert(0, project_root)
 from reports.bed_metrics import plot_metric_heatmap, bed_metric_table  # noqa
 
 
-def process_hist_mod(peaks_root):
+def process_hist_mod(peaks_root, threads):
     all_peaks_root = peaks_root / "bed_all"
     peaks_paths = list(all_peaks_root.glob("*Peak"))
 
     df_path = all_peaks_root / "intersection.csv"
     if df_path.exists():
-        df = pd.DataFrame.from_csv(df_path)
+        df = pd.DataFrame.from_csv(str(df_path))
     else:
-        df = bed_metric_table(peaks_paths, peaks_paths, threads=30)
+        df = bed_metric_table(peaks_paths, peaks_paths, threads=threads)
         df.to_csv(str(df_path))
 
     print(df.head())
@@ -28,6 +28,6 @@ def process_hist_mod(peaks_root):
 
 
 h3k4me3 = Path("/mnt/stripe/bio/experiments/aging/peak_calling/H3K4me3")
-process_hist_mod(h3k4me3)
+process_hist_mod(h3k4me3, threads=30)
 
 # if __name__ == "__main__":
