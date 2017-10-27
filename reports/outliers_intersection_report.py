8dff8e617096d790b7e73109b23c539536b33316
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-10-27 16:01:41 +0300

    cleanup

--- reports/outliers_intersection_report.py
+++ reports/outliers_intersection_report.py
@@ -57,19 +57,6 @@ def process_hist_mod(peaks_root, threads, golden, pdf_printer):
         row_label_fun=donor_name, col_label_fun=donor_name
     )
 
-    # print to png:
-    result_plot_path = str(peaks_root / "{}.png".format(res_prefix))
-    plot_metric_heatmap("Intersection metric: All donors {}".format(
-        peaks_root.name
-    ),
-        df,
-        save_to=result_plot_path,
-        row_cluster=True, col_cluster=True,
-        row_color_fun=donor_color, col_color_fun=donor_color,
-        row_label_fun=donor_name, col_label_fun=donor_name
-    )
-    print("Metrics plot saved to:", str(result_plot_path))
-
 
 golden_root = Path("/mnt/stripe/bio/experiments/aging/peak_calling")
 result_plot_path = golden_root / "intersection.pdf"
