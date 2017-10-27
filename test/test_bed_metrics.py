fdbf2696c0cb5a1b4c10fc5536ed01b6d026034b
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-10-27 15:27:03 +0300

    test fixed for mac

--- test/test_bed_metrics.py
+++ test/test_bed_metrics.py
@@ -92,6 +92,11 @@ def test_heatmap_donor_color_fun(name, color):
     assert color == res[0][1]
 
 
+# To replace images use:
+# cd ~/work/washu/test/testdata/metrics
+# find . -name "test_plot*.png" | xargs -I fname bash -c "echo fname |
+#    sed s/test_plot.*#//g | sed s/.$PLATFORM_SRC$.png//g |
+#    xargs -I nname cp fname nname.$PLATFORM_TARGET$.png"
 def assert_image(expected_path, actual_path):
     os_platform = platform.system().lower()
     assert os_platform in ["linux", "darwin"], \
@@ -122,7 +127,7 @@ def assert_image(expected_path, actual_path):
             export_dir = os_specific_path.parent
 
         export_dir.mkdir(exist_ok=True, parents=True)
-        export_path = str(export_dir / (prefix + "-" + exp_name))
+        export_path = str(export_dir / (prefix + "#" + exp_name))
         shutil.copy(actual_path, export_path)
         print("Mismatched image copied to:", str(export_path),
               file=sys.stderr)
