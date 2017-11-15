ea00e38d2ce01ee7d9fcdd1b340db6fde9aa0d23
Evgeny Kurbatsky <evgeny.kurbatsky@jetbrains.com>
2017-11-15 14:13:42 +0300

    cleanup copy paste

--- scripts/run_diffbind.sh
+++ scripts/run_diffbind.sh
@@ -2,13 +2,10 @@
 
 # Check tools
 which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
-which macs2 &>/dev/null || { echo "macs2 not found! Install macs2: <https://github.com/taoliu/MACS/wiki/Install-macs2>"; exit 1; }
 
 # Load technical stuff
 source $(dirname $0)/../parallel/util.sh
 SCRIPT_DIR="$(project_root_dir)"
-export TMPDIR=$(type job_tmp_dir &>/dev/null && echo "$(job_tmp_dir)" || echo "/tmp")
-mkdir -p "${TMPDIR}"
 
 ################################################################################
 # Configuration start ##########################################################
@@ -47,17 +44,3 @@ Rscript ${SCRIPT_DIR}/R/diffbind.R ${NAME}.csv
 SCRIPT
 
 wait_complete "$QSUB_ID"
-
-# Filter out old and young donors and sort by Q-Value
-cat ${NAME}_result.csv | awk 'NR > 1 {print $0}' | sed 's#"##g' | tr ',' '\t' |\
-awk -v OFS='\t' '$9 > 0 {print $0}' | sort -T ${TMPDIR} -k10,10g > ${NAME}_cond1.bed
-
-cat ${NAME}_result.csv | awk 'NR > 1 {print $0}' | sed 's#"##g' | tr ',' '\t' |\
-awk -v OFS='\t' '$9 < 0 {print $0}' | sort -T ${TMPDIR} -k10,10g > ${NAME}_cond2.bed
-
-# Save ${NAME} results to simple BED3 format
-awk -v OFS='\t' '{print $1,$2,$3}' ${NAME}_cond1.bed > ${NAME}_cond1.bed3
-awk -v OFS='\t' '{print $1,$2,$3}' ${NAME}_cond2.bed > ${NAME}_cond2.bed3
-
-# TMP dir cleanup:
-type clean_job_tmp_dir &>/dev/null && clean_job_tmp_dir
