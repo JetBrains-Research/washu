6a48ae4a9e78244999417afd20c082f742952cf7
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-11-16 17:28:01 +0300

    loci@peaks stats analysis

--- reports/loci_intersection_report.py
+++ reports/loci_intersection_report.py
@@ -5,6 +5,7 @@ import datetime
 
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 
 def _cli():
@@ -36,7 +37,7 @@ def _cli():
     results_dir.mkdir(parents=True, exist_ok=True)
     ########################################################################
 
-    loi_dict = loi.collect_loci(loci_root)
+    loci_dict = loi.collect_loci(loci_root)
 
     # default peaks
     golden_peaks_root = data_root / "experiments/aging/peak_calling"
@@ -61,27 +62,27 @@ def _cli():
     ########################################################################
 
     print("----- [Report]: Default ----")
-    report_default(loi_dict, results_dir, threads)
+    report_default(loci_dict, results_dir, threads)
     print("----- [Report]: Consensus ----")
-    report_consensus(loi_dict, results_dir, threads)
+    report_consensus(loci_dict, results_dir, threads)
 
     for key in sorted(peaks_map.keys()):
         print("----- [Report]: Donors {} ----".format(key))
-        report_donors(key, peaks_map, loi_dict, results_dir, threads, outliers_df)
+        report_donors(key, peaks_map, loci_dict, results_dir, threads, outliers_df)
 
     print("----- [Report]: Repeats ----")
-    report("repeats", loi_dict, results_dir, threads,
+    report("repeats", loci_dict, results_dir, threads,
            consensus_yo=False, default=False)
     print("----- [Report]: TFs ----")
-    report("tfs", loi_dict, results_dir, threads,
+    report("tfs", loci_dict, results_dir, threads,
            consensus_yo=False, default=False)
     print("----- [Report]: Differential ChipSeq ----")
-    report("chipseq_diff_loci", loi_dict, results_dir, threads)
+    report("chipseq_diff_loci", loci_dict, results_dir, threads)
     print("----- [Report]: Differential RnaSeq ----")
-    report("rna_diff", loi_dict, results_dir, threads)
+    report("rna_diff", loci_dict, results_dir, threads)
 
     print("----- [Report]: Pathways interesting ----")
-    report("interesting_pathways", loi_dict, results_dir, threads,
+    report("interesting_pathways", loci_dict, results_dir, threads,
            key_side_size=200,
            itself=False, chromhmm=False, default=False, repeats=False, consensus=False)
 
@@ -92,11 +93,19 @@ def _cli():
     notch_pathways.extend(
         [p for p in (loci_root / "interesting_pathways").glob('R-HSA-264460*_cds.bed')])
     notch_pathways.sort(key=lambda p: p.name)
-    loi_dict["notch_pathways"] = notch_pathways
+    loci_dict["notch_pathways"] = notch_pathways
 
-    report("notch_pathways", loi_dict, results_dir, threads,
+    report("notch_pathways", loci_dict, results_dir, threads,
            key_side_size=20,
            itself=False, chromhmm=False, default=False, repeats=False, consensus=False)
+
+    # Stat tests:
+    for key in ["zinbra_tuned", "golden_tuned"]:
+        print("----- [Stat tests]: Donors {} ----".format(key))
+        for loci_key in loci_dict:
+            test_donors(key, peaks_map, loci_dict, loci_key, results_dir, threads, outliers_df)
+        # test_donors(key, peaks_map, loci_dict, "default", results_dir, threads, outliers_df)
+        break
 
 
 def _adjustment():
@@ -107,12 +116,12 @@ def _adjustment_wrc():
     return dict(left=0.15, top=0.95, right=0.62, bottom=0.3)
 
 
-def report_default(loi_dict, outdir, threads):
+def report_default(loci_dict, outdir, threads):
     result_plot_path = outdir / "default.pdf"
     with PdfPages(str(result_plot_path)) as pdf:
         init_pdf_info(pdf)
         process_intersection_metric(
-            loi_dict['default'], loi_dict['default'],
+            loci_dict['default'], loci_dict['default'],
             outdir / "default.csv", pdf,
             adjustments=_adjustment(),
             col_label_converter=loi.label_converter_shorten_loci,
@@ -120,7 +129,7 @@ def report_default(loi_dict, outdir, threads):
             row_cluster=False, col_cluster=False, threads=threads, figsize=(20, 16))
 
         process_intersection_metric(
-            loi_dict['default'], loi_dict['chromhmm'],
+            loci_dict['default'], loci_dict['chromhmm'],
             outdir / "default@chromhmm.csv", pdf,
             adjustments=_adjustment(),
             col_label_converter=loi.label_converter_shorten_loci,
@@ -128,17 +137,17 @@ def report_default(loi_dict, outdir, threads):
             row_cluster=False, col_cluster=False, threads=threads, figsize=(15, 15))
 
 
-def report_consensus(loi_dict, outdir, threads, consensus_type="median_consensus"):
+def report_consensus(loci_dict, outdir, threads, consensus_type="median_consensus"):
     result_plot_path = outdir / "{}.pdf".format(consensus_type)
     with PdfPages(str(result_plot_path)) as pdf:
         init_pdf_info(pdf)
         consensus = sorted(
-            loi_dict['zinbra_{}'.format(consensus_type)] + loi_dict['golden_{}'.format(
+            loci_dict['zinbra_{}'.format(consensus_type)] + loci_dict['golden_{}'.format(
                 consensus_type)],
             key=lambda f: f.name
         )
         process_intersection_metric(
-            consensus, loi_dict['default'],
+            consensus, loci_dict['default'],
             outdir / "{}@default.csv".format(consensus_type), pdf,
             col_label_converter=loi.label_converter_shorten_loci,
             row_label_converter=loi.label_converter_shorten_loci,
@@ -146,7 +155,7 @@ def report_consensus(loi_dict, outdir, threads, consensus_type="median_consensus
             row_cluster=False, col_cluster=True, threads=threads, figsize=(20, 15))
 
         process_intersection_metric(
-            consensus, loi_dict['chromhmm'],
+            consensus, loci_dict['chromhmm'],
             outdir / "{}@chromhmm.csv".format(consensus_type), pdf,
             adjustments=_adjustment(),
             col_label_converter=loi.label_converter_shorten_loci,
@@ -162,7 +171,7 @@ def report_consensus(loi_dict, outdir, threads, consensus_type="median_consensus
             row_cluster=False, col_cluster=False, threads=threads, figsize=(20, 15))
 
         process_intersection_metric(
-            consensus, loi_dict['repeats'],
+            consensus, loci_dict['repeats'],
             outdir / "{}@repeats.csv".format(consensus_type), pdf,
             col_label_converter=loi.label_converter_shorten_loci,
             row_label_converter=loi.label_converter_shorten_loci,
@@ -180,7 +189,7 @@ def report_consensus(loi_dict, outdir, threads, consensus_type="median_consensus
             row_cluster=False, col_cluster=False, threads=threads, figsize=(10, 10))
 
 
-def report_donors(tool, peaks_map, loi_dict, outdir, threads, outliers_df):
+def report_donors(tool, peaks_map, loci_dict, outdir, threads, outliers_df):
     peaks_dict = peaks_map[tool]
 
     result_plot_path = outdir / "{}_by_donor.pdf".format(tool)
@@ -194,7 +203,7 @@ def report_donors(tool, peaks_map, loi_dict, outdir, threads, outliers_df):
             annotator = None if not anns else bm.color_annotator_chain(*anns)
 
             process_intersection_metric(
-                peaks_dict[hist], loi_dict['default'],
+                peaks_dict[hist], loci_dict['default'],
                 outdir / "{}_{}@default.csv".format(tool, hist), pdf,
                 adjustments=dict(left=0.15, top=0.95, right=0.9, bottom=0.3),
                 row_cluster=False, col_cluster=False, threads=threads, figsize=(20, 15),
@@ -204,7 +213,7 @@ def report_donors(tool, peaks_map, loi_dict, outdir, threads, outliers_df):
             )
 
 
-def report(key, loi_dict, outdir, threads, key_side_size=15, consensus_type="median_consensus",
+def report(key, loci_dict, outdir, threads, key_side_size=15, consensus_type="median_consensus",
            itself=True, chromhmm=True, default=True, repeats=True, consensus=True,
            consensus_yo=True):
     result_plot_path = outdir / "{}.pdf".format(key)
@@ -213,7 +222,7 @@ def report(key, loi_dict, outdir, threads, key_side_size=15, consensus_type="med
         init_pdf_info(pdf)
         if itself:
             process_intersection_metric(
-                loi_dict[key], loi_dict[key],
+                loci_dict[key], loci_dict[key],
                 outdir / "{}.csv".format(key), pdf,
                 adjustments=_adjustment_wrc(),
                 col_label_converter=loi.label_converter_shorten_loci,
@@ -232,7 +241,7 @@ def report(key, loi_dict, outdir, threads, key_side_size=15, consensus_type="med
 
         for loci_key, loci_side_size in loci:
             process_intersection_metric(
-                loi_dict[key], loi_dict[loci_key],
+                loci_dict[key], loci_dict[loci_key],
                 outdir / "{}@{}.csv".format(key, loci_key), pdf,
                 adjustments=_adjustment() if loci_key not in {"default"} else _adjustment_wrc(),
                 col_label_converter=loi.label_converter_shorten_loci,
@@ -241,7 +250,7 @@ def report(key, loi_dict, outdir, threads, key_side_size=15, consensus_type="med
                 figsize=(loci_side_size, key_side_size))
 
             process_intersection_metric(
-                loi_dict[loci_key], loi_dict[key],
+                loci_dict[loci_key], loci_dict[key],
                 outdir / "{}@{}.csv".format(loci_key, key), pdf,
                 adjustments=_adjustment() if key not in {"default"} else _adjustment_wrc(),
                 col_label_converter=loi.label_converter_shorten_loci,
@@ -251,13 +260,13 @@ def report(key, loi_dict, outdir, threads, key_side_size=15, consensus_type="med
 
         # consensus
         consensus_paths = sorted(
-            loi_dict['zinbra_{}'.format(consensus_type)] + loi_dict['golden_{}'.format(
+            loci_dict['zinbra_{}'.format(consensus_type)] + loci_dict['golden_{}'.format(
                 consensus_type)],
             key=lambda f: f.name
         )
         if consensus:
             process_intersection_metric(
-                loi_dict[key], consensus_paths,
+                loci_dict[key], consensus_paths,
                 outdir / "{}@{}.csv".format(key, consensus_type), pdf,
                 adjustments=_adjustment(),
                 col_label_converter=loi.label_converter_shorten_loci,
@@ -269,7 +278,7 @@ def report(key, loi_dict, outdir, threads, key_side_size=15, consensus_type="med
             consensus_yo_paths\
                 = [p for p in consensus_paths if "DS" in p.name and "without" not in p.name]
             process_intersection_metric(
-                loi_dict[key], consensus_yo_paths,
+                loci_dict[key], consensus_yo_paths,
                 outdir / "{}@{}_yo.csv".format(key, consensus_type), pdf,
                 adjustments=_adjustment(),
                 col_label_converter=loi.label_converter_shorten_loci,
@@ -297,6 +306,188 @@ def init_pdf_info(pdf):
     d['ModDate'] = datetime.datetime.today()
 
 
+def _pvalues_above_thr(thr005, thr001):
+    def inner(label):
+        anns = []
+        if label in thr005:
+            anns.append(("0.05", "blue"))
+        else:
+            anns.append(("0.05", "lightgray"))
+
+        if label in thr001:
+            anns.append(("0.01", "green"))
+        else:
+            anns.append(("0.01", "lightgray"))
+        return anns
+
+    return inner
+
+
+def test_donors(tool, peaks_map, loci_dict, loci_key, outdir, threads, outliers_df):
+    ha = "two-sided"  # 'less', 'two-sided', or 'greater'
+
+    ################################################################################
+    peaks_dict = peaks_map[tool]
+    tool = tool or "all"  # presentable text for labels, we use 'None' for all loci
+
+    loci_paths = loci_dict[loci_key]
+
+    for hist in sorted(peaks_dict.keys()):
+        peaks_paths = peaks_dict[hist]
+
+        result_plot_path = outdir / "{}_{}@{}_stat.pdf".format(tool, hist, loci_key)
+        stats_df_path = outdir / "{}_{}@{}_stat.csv".format(tool, hist, loci_key)
+        df = bm.load_or_build_metrics_table(
+            peaks_paths, loci_paths,
+            outdir / "{}_{}@{}.csv".format(tool, hist, loci_key),
+            threads=threads
+        )
+
+        # Split: Old / Young donors
+        donors_age_id = [loi.donor_order_id(p) for p in peaks_paths]
+        groups = np.asarray([age for age, _id in donors_age_id])
+        mask_od_group = groups == "OD"
+        mask_yd_group = groups == "YD"
+        print("    Peaks: [{}], OD: [{}], YD: [{}]".format(
+            len(peaks_paths), np.sum(mask_od_group), np.sum(mask_yd_group)
+        ))
+
+        # Load Outliers info
+        mask_not_outlier = None
+        if hist and outliers_df is not None:
+            if hist in outliers_df.columns:
+                col = outliers_df.loc[:, hist]
+                outliers_codes = [col["{}{}".format(age, id)] for age, id in donors_age_id]
+                mask_not_outlier = np.asarray(outliers_codes) == 0
+        if mask_not_outlier is None:
+            print("    {}: No outliers info, use all donors".format(hist))
+            mask_not_outlier = np.ones((len(peaks_paths), 1), dtype=bool)
+
+        print("    Not outliers: [{}]".format(np.sum(mask_not_outlier)))
+
+        # Young/old group without outliers
+        mask_od_group = mask_od_group * mask_not_outlier
+        mask_yd_group = mask_yd_group * mask_not_outlier
+        print("    Peaks: OD w/o outliers: [{}], YD w/o outliers: [{}]".format(
+            np.sum(mask_od_group),
+            np.sum(mask_yd_group))
+        )
+
+        # Stat test for each locus: Old vs Young
+        from scipy.stats import mannwhitneyu
+        from statsmodels.stats.multitest import multipletests
+
+        df_ods = df[mask_od_group]
+        df_yds = df[mask_yd_group]
+        print("    Dfs: OD = {}, YD = {}".format(df_ods.shape, df_yds.shape))
+
+        pvalues = []
+        for i in range(df.shape[1]):
+            try:
+                pvalues.append(
+                    mannwhitneyu(df_ods.iloc[:, i], df_yds.iloc[:, i], alternative=ha).pvalue
+                )
+            except ValueError as e:
+                print("Error: {} in file:\n{}".format(e, df.columns[i]))
+                # TODO: problem_files.append(file)
+                pvalues.append(np.nan)
+
+        loci_pvalues_df = pd.DataFrame.from_dict(dict(loci=df.columns, pvalue=pvalues))
+        loci_pvalues_df.index = loci_pvalues_df.loci
+        loci_pvalues_df.drop("loci", inplace=True, axis=1)
+
+        print("Not corrected pvalue, first 10 lowest pvalues:")
+        print(loci_pvalues_df.sort_values(by="pvalue").head(10))
+
+        # P-values correction
+        #   see: http://www.statsmodels.org/dev/_modules/statsmodels/stats/multitest.html
+        pvalues_col = loci_pvalues_df["pvalue"]
+        pvalues_not_nan_mask = ~np.isnan(pvalues_col)
+
+        _reject, pvals_corrected, *_ = multipletests(
+            pvals=pvalues_col[pvalues_not_nan_mask],
+            # fdr_bh, holm-sidak, bonferroni
+            alpha=0.05, method="fdr_bh"
+        )
+
+        loci_pvalues_df["dfr_bh"] = np.nan
+        loci_pvalues_df.loc[pvalues_not_nan_mask, "dfr_bh"] = pvals_corrected
+
+        # Save results:
+        loci_pvalues_df.to_csv(str(stats_df_path))
+        # print(loci_pvalues_df)
+
+        # Plots
+        with PdfPages(str(result_plot_path)) as pdf:
+            init_pdf_info(pdf)
+
+            # TODO: to one plot: pvalues + colors for adjusted threshold ?
+            manhattan_plot(loci_pvalues_df.sort_values(by="pvalue"), "pvalue",
+                           save_to=pdf, xticks=False)
+
+            manhattan_plot(loci_pvalues_df.sort_values(by="dfr_bh"), "dfr_bh",
+                           correction="Benjamini–Hochberg corrected",
+                           save_to=pdf, xticks=False)
+
+            # Heatmap with selected cols:
+            anns = [bm.color_annotator_age]
+            if hist and outliers_df is not None:
+                if hist in outliers_df.columns:
+                    anns.append(bm.color_annotator_outlier(outliers_df, hist))
+            row_annotator = None if not anns else bm.color_annotator_chain(*anns)
+
+            def loci_passed_thr(df, col, thr):
+                return set(df.index[df[col] < thr].tolist())
+
+            def plot_significant(col, title):
+                thr001 = loci_passed_thr(loci_pvalues_df, col, 0.01)
+                thr005 = loci_passed_thr(loci_pvalues_df, col, 0.05)
+                thr01 = loci_passed_thr(loci_pvalues_df, col, 0.1)
+                print("Loci passing", title, "threshold", col, len(thr01))
+                if thr01:
+                    bm.plot_metric_heatmap(
+                        "IM {} with for {} < 0.1".format(stats_df_path.name, title),
+                        df.loc[:, thr01],
+                        save_to=pdf,
+                        adjustments=dict(left=0.15, top=0.95, right=0.9, bottom=0.3),
+                        row_cluster=False, col_cluster=False, threads=threads, figsize=(20, 15),
+                        col_color_annotator=_pvalues_above_thr(thr005, thr001),
+                        row_color_annotator=row_annotator,
+                        col_label_converter=loi.label_converter_shorten_loci,
+                        row_label_converter=bm.label_converter_donor_and_tool,
+                    )
+
+            plot_significant("pvalue", "not-adjusted pvalues")
+            plot_significant("dfr_bh", "adjusted pvalues (BH fdr)")
+    pass
+
+
+def manhattan_plot(pvalues_df, col_name, correction="Uncorrected",
+                   save_to=None, adjustments=None, xticks=True):
+    plt.figure(figsize=(15, 8))
+    n = pvalues_df.shape[0]
+
+    ax = plt.subplot()
+    ax.plot(range(n), 1/pvalues_df[col_name], marker=".", ls="")
+    # ax.plot(range(n), 1/pvalues_df["pvalue"], marker=".", ls="")
+    # ax.axhline(y=-np.log10(0.05), xmin=0, xmax=n, color="r", linestyle='dotted')
+    ax.axhline(y=1/0.05, xmin=0, xmax=n, color="r", linestyle='dotted')
+    ax.set_ylabel("{} pvalues (-log(p) scale )".format(correction))
+    ax.set_yscale("log")
+    ax.set_title("[Peaks@loci] Mann whitney u test pvalues")
+    if xticks:
+        plt.xticks(range(n),
+                   [loi.label_converter_shorten_loci(l) for l in pvalues_df.index],
+                   rotation='vertical')
+
+    adjustments = adjustments or {}
+    plt.subplots_adjust(left=adjustments.get('left', 0.2),
+                        right=adjustments.get('right', 0.8),
+                        top=adjustments.get('top', 0.8),
+                        bottom=adjustments.get('bottom', 0.3))
+    bm.save_plot(save_to)
+
+
 if __name__ == "__main__":
     parent_dir = os.path.dirname(os.path.realpath(__file__))
     project_root = os.path.abspath(os.path.join(parent_dir) + "/..")
@@ -305,6 +496,7 @@ if __name__ == "__main__":
     # Force matplotlib to not use any Xwindows backend.
     import matplotlib
     matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
 
     import reports.bed_metrics as bm
     import reports.loci_of_interest as loi
