e3768c2d0fdf2302616b331cb727b65471dd42fb
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-11-21 21:35:03 +0300

    bugs fixed

--- bed/benchmark_consensus.sh
+++ bed/benchmark_consensus.sh
@@ -12,16 +12,18 @@ fi
 BENCHMARK_ROOT=$1
 LOCI_ROOT=$2
 
+mkdir -p ${LOCI_ROOT}
 cd ${BENCHMARK_ROOT}
-for HIST_DIR in $(find . -type d -name "H*" -maxdepth 1); do
+for HIST_DIR in $(find . -maxdepth 1 -type d -name "H*"); do
     cd ${HIST_DIR}
     HIST_NAME=${HIST_DIR##*/}
     # We decided to exclude MACS2 narrow peaks
     # Sicer not ready yet, let's exclude it too
-    for DIR in $(find . -type d -maxdepth 1 ! -path . ! -path "./macs_narrow" ! -path "./sicer"); do
-        /home/user/work/tsurinov/washu/bed/consensus.sh -p 50 ${DIR} ${HIST_NAME}
-        # "$(project_root_dir)/bed/consensus.sh" -p 50  ${DIR} ${HIST_NAME}
-        cp "${DIR}/*_consensus.bed" ${LOCI_ROOT} ${HIST_NAME}
+    for DIR in $(find . -maxdepth 1 -type d ! -path . ! -path "./macs_narrow" ! -path "./sicer"); do
+        echo "Processing: $(expand_path ${DIR})"
+        bash /home/user/work/tsurinov/washu/bed/consensus.sh -p 50 ${DIR} ${HIST_NAME}
+        # bash "$(project_root_dir)/bed/consensus.sh" -p 50  ${DIR} ${HIST_NAME}
+        find ${DIR} -maxdepth 1 -name "*_consensus.bed" | xargs -I fname cp fname ${LOCI_ROOT}
     done
 done
 
