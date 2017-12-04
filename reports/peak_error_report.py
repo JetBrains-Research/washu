dbeb7653d78abceddbe12b92539744cd3173e337
Petr Tsurinov <Petr.Tsurinov@jetbrains.com>
2017-12-04 16:57:39 +0300

    Updated script for error plots creation.

--- reports/peak_error_report.py
+++ reports/peak_error_report.py
@@ -9,14 +9,20 @@ import numpy as np
 
 __author__ = 'petr.tsurinov@jetbrains.com'
 help_data = """
+Script creates pdf report with error plots from two selected tools tuning parameters files:
+ 1) Total error
+ 2) Peak start error
+ 3) Peak end error
+ 4) No peaks error
+ 5) Peaks error
 """
 
 
 def _cli():
     parser = argparse.ArgumentParser(description=help_data,
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("parameters1", help="Peaks folder")
-    parser.add_argument("parameters2", help="Peaks folder")
+    parser.add_argument("parameters1", help="First tool parameters file")
+    parser.add_argument("parameters2", help="Second tool parameters file")
     parser.add_argument("output", help="Output pdf path")
 
     args = parser.parse_args()
@@ -25,7 +31,7 @@ def _cli():
     pdf_path = args.output
 
     parameters1 = pd.DataFrame.from_csv(path=str(parameters_path1), index_col=None, header=1,
-                                       sep="\t")
+                                        sep="\t")
     parameters1 = parameters1.drop_duplicates(['name', 'error'], keep='first')
     parameters2 = pd.DataFrame.from_csv(path=str(parameters_path2), index_col=None, header=1,
                                         sep="\t")
@@ -36,25 +42,14 @@ def _cli():
     with PdfPages(pdf_path) as pdf:
         plt.figure(figsize=(10, 15))
 
-        plt.subplot(5, 1, 1)
-        error_plot(ind, parameters_path1, parameters1["error"], parameters_path2,
-                   parameters2["error"], parameters1["name"].values, 'Total error')
-
-        plt.subplot(5, 1, 2)
-        error_plot(ind, parameters_path1, parameters1["error_S"], parameters_path2,
-                   parameters2["error_S"], parameters1["name"].values, 'Peak start error')
-
-        plt.subplot(5, 1, 3)
-        error_plot(ind, parameters_path1, parameters1["error_E"], parameters_path2,
-                   parameters2["error_E"], parameters1["name"].values, 'Peak end error')
-
-        plt.subplot(5, 1, 4)
-        error_plot(ind, parameters_path1, parameters1["error_N"], parameters_path2,
-                   parameters2["error_N"], parameters1["name"].values, 'No peaks error')
-
-        plt.subplot(5, 1, 5)
-        error_plot(ind, parameters_path1, parameters1["error_P"], parameters_path2,
-                   parameters2["error_P"], parameters1["name"].values, 'Peaks error')
+        for index, (error, label) in enumerate([("error", 'Total error'),
+                                                ("error_S", 'Peak start error'),
+                                                ("error_E", 'Peak end error'),
+                                                ("error_N", 'No peaks error'),
+                                                ("error_P", 'Peaks error')]):
+            plt.subplot(5, 1, index + 1)
+            error_plot(ind, parameters_path1, parameters1[error], parameters_path2,
+                       parameters2[error], parameters1["name"].values, label)
 
         plt.tight_layout()
         save_plot(pdf)
