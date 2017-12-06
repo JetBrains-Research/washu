03c55d2fbab906fb71dc1fd993cb900ead3b80eb
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2017-12-06 18:41:57 +0300

    Fix pbc_nrf computation in case of empty TwoPair metrics

--- reports/pbc_nrf.sh
+++ reports/pbc_nrf.sh
@@ -43,18 +43,21 @@ else
     fi
 fi
 
-# PBC File output:
-#   TotalReadPairs <tab>
-#   DistinctReadPairs <tab>
-#   OneReadPair <tab>
-#   TwoReadPairs <tab>
-#   NRF=Distinct/Total <tab>
-#   PBC1=OnePair/Distinct <tab>
-#   PBC2=OnePair/TwoPair
+T=$'\t'
+>&2 echo "TotalReadPairs${T}DistinctReadPairs${T}OneReadPair${T}TwoReadPairs${T}\
+NRF=Distinct/Total${T}PBC1=OnePair/Distinct${T}PBC2=OnePair/TwoPair"
+
 cat ${PILEUP_BED} | grep -E 'chr[0-9XY]+' | \
     awk  -v OFS='\t' '{print $1,$2,$3,$6}' | \
     sort | uniq -c | \
-    awk 'BEGIN{mt=0;m0=0;m1=0;m2=0} ($1==1){m1=m1+1} ($1==2){m2=m2+1} {m0=m0+1} {mt=mt+$1} END{printf "%d\t%d\t%d\t%d\t%f\t%f\t%f\n",mt,m0,m1,m2,m0/mt,m1/m0,m1/m2}'
+    awk 'BEGIN{mt=0;m0=0;m1=0;m2=0}
+    ($1==1){m1=m1+1} ($1==2){m2=m2+1} {m0=m0+1} {mt=mt+$1}
+    END{
+        if (mt!=0){m0_t=m0/mt} else {m0_t=-1.0};
+        if (m0!=0){m1_0=m1/m0} else {m1_0=-1.0};
+        if (m2!=0){m1_2=m1/m2} else {m1_2=-1.0};
+        printf "%d\t%d\t%d\t%d\t%f\t%f\t%f\n",mt,m0,m1,m2,m0_t,m1_0,m1_2;
+    }'
 
 # Cleanup
 type clean_job_tmp_dir &>/dev/null && clean_job_tmp_dir
