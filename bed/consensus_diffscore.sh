6a4dbe935f857a8bfe786af1b94aacb2620273c1
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2017-12-06 19:24:10 +0300

    missed file

--- bed/consensus_diffscore.sh
+++ bed/consensus_diffscore.sh
@@ -8,6 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 FOLDER=$1
+THR=$2
 cd ${FOLDER}
 
 OD_FILES="$(find . -maxdepth 1 \( -wholename "*OD*island.bed" -or -wholename "*OD*Peak" -or \
@@ -19,9 +20,11 @@ YD_FILES="$(find . -maxdepth 1 \( -wholename "*YD*island.bed" -or -wholename "*Y
 #echo "OD: "${OD_FILES}
 #echo "YD: "${YD_FILES}
 N_OD="$(echo "${OD_FILES}" | wc -l)"
+
 echo "chr"$'\t'"start"$'\t'"end"$'\t'"cons"$'\t'"od_cons"$'\t'"yd_cons"$'\t'"absdiff"
-bedtools multiinter -v -i ${OD_FILES} ${YD_FILES} | awk -v K="${N_OD}" 'function abs(v)
-{return v < 0 ? -v : v}{
- ods=0; yds=0; for (i=6; i<6+K; i++) ods+=$i;
- for (i=6+K; i<=NF; i++) yds+=$i;
- print $1, $2, $3, $4, ods, yds, abs(ods-yds) }'
+bedtools multiinter -v -i ${OD_FILES} ${YD_FILES} | awk -v K="${THR}" '{ if ($4 >= K) print $0 }' |
+ awk -v OFS='\t' -v K="${N_OD}" '{ ods=0; yds=0; for (i=6; i<6+K; i++) ods+=$i;
+for (i=6+K; i<=NF; i++) yds+=$i; print $1, $2, $3, $4, ods, yds}' |
+ bedtools merge -c 4,5,6 -o max,max,max |
+ awk 'function abs(v) {return v < 0 ? -v : v} {print $0"\t"abs($5-$6)}'
+
