9664f57850d54a7782fead14898548fa6a731905
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2018-01-18 13:09:51 +0300

    Since we have FRiP based normalization, RPM/RPKM peaks is useless

--- downstream/signals/signals.py
+++ downstream/signals/signals.py
@@ -13,14 +13,13 @@ from scripts.util import *
 from downstream.aging import *
 
 
-def process(data_path, sizes_path, peaks_sizes_path, post_process):
+def process(data_path, sizes_path, peaks_sizes_path, post_process_callback):
     data = pd.read_csv(data_path, sep='\t',
                        names=('chr', 'start', 'end', 'coverage', 'mean0', 'mean', 'name'))
 
     processing_chipseq = not [n for n in data['name'] if re.match('.*(meth|trans|mirna).*', n)]
-
     if processing_chipseq:
-        print("Processing ChIP-Seq, input found")
+        print("Processing ChIP-Seq")
     else:
         print("Processing Methylation|Transcription|miRNA, input not found")
 
@@ -32,12 +31,12 @@ def process(data_path, sizes_path, peaks_sizes_path, post_process):
     raw_path = re.sub('.tsv', '_raw.tsv', data_path)
     df_raw.to_csv(raw_path, sep='\t')
     print('Saved RAW signal to {}'.format(raw_path))
-    post_process(raw_path)
+    post_process_callback(raw_path)
 
     print("Processing QUANTILE normalization")
     quantile_path = re.sub('.tsv', '_q.tsv', data_path)
     process_quantile(quantile_path, df_raw)
-    post_process(quantile_path)
+    post_process_callback(quantile_path)
 
     # Normalization is available only for ChIP-Seq
     if not processing_chipseq:
@@ -45,50 +44,32 @@ def process(data_path, sizes_path, peaks_sizes_path, post_process):
 
     print('Processing RPM normalization')
     sizes = pd.read_csv(sizes_path, sep='\t', names=('name', 'size'), index_col='name')
-    sizes_pm = sizes / 1000000
+    sizes_pm = sizes / 1000000.0
     sizes_pm.columns = ['size_pm']
     data = pd.merge(data, sizes_pm, left_on="name", how='left', right_index=True)
 
     data['rpm'] = data['coverage'] / data['size_pm']
     rpm_path = re.sub('.tsv', '_rpm.tsv', data_path)
     save_signal(rpm_path, data, 'rpm', 'Saved RPM')
-    post_process(rpm_path)
+    post_process_callback(rpm_path)
 
     print('Processing RPKM normalization')
     data['rpk'] = (data['end'] - data['start']) / 1000.0
     data['rpkm'] = data['rpm'] / data['rpk']
-    save_signal(re.sub('.tsv', '_rpkm.tsv', data_path), data, 'rpkm', 'Saved RPKM')
+    rpkm_path = re.sub('.tsv', '_rpkm.tsv', data_path)
+    save_signal(rpkm_path, data, 'rpkm', 'Saved RPKM')
+    post_process_callback(rpkm_path)
 
+    if peaks_sizes_path and os.path.exists(peaks_sizes_path):
+        print('Processing FRIP normalization')
+        norm_path = re.sub('.tsv', '_frip.tsv', data_path)
+        process_frip(norm_path, data, sizes_path, peaks_sizes_path)
+        post_process_callback(norm_path)
+
+    print('Processing DiffBind scores')
     raw_data = pd.pivot_table(data, index=['chr', 'start', 'end'],
                               columns='name', values='coverage', fill_value=0)
-    process_diffbind_scores(data_path, raw_data, sizes, post_process)
-
-    if not (peaks_sizes_path and os.path.exists(peaks_sizes_path)):
-        return
-
-    print('Processing peaks based normalization')
-    norm_path = re.sub('.tsv', '_norm.tsv', data_path)
-    process_norm(norm_path, data, sizes_path, peaks_sizes_path)
-    post_process(norm_path)
-
-    print('Processing RPM_PEAKS normalization')
-    peaks_sizes = pd.read_csv(peaks_sizes_path, sep='\t', names=('name', 'sizes_peaks_pm'),
-                              index_col='name')
-    sizes_peaks_pm = peaks_sizes / 1000000
-
-    data = pd.merge(data, sizes_peaks_pm, left_on="name", how='left', right_index=True)
-    data['rpm_peaks'] = data['coverage'] / data['sizes_peaks_pm']
-    rpm_peaks_path = re.sub('.tsv', '_rpm_peaks.tsv', data_path)
-    save_signal(rpm_peaks_path, data, 'rpm_peaks',
-                'Saved normalized reads by RPM reads in peaks signal')
-    post_process(rpm_peaks_path)
-
-    print('Processing RPKM_PEAKS normalization')
-    data['rpkm_peaks'] = data['rpm_peaks'] / data['rpk']
-    rpkm_peaks_path = re.sub('.tsv', '_rpkm_peaks.tsv', data_path)
-    save_signal(rpkm_peaks_path, data, 'rpkm_peaks',
-                'Saved normalized reads by RPKM reads in peaks signal')
-    post_process(rpkm_peaks_path)
+    process_diffbind_scores(data_path, raw_data, sizes, post_process_callback)
 
 
 def save_signal(path, data, signal_type, msg):
@@ -106,22 +87,26 @@ def quantile_normalize_using_target(x, target):
 
 def process_quantile(path, df_raw):
     # Normalize everything to the first track
-    for c in df_raw.columns[1:]:
+    signal_columns = [c for c in df_raw.columns if not is_input(c)]
+    for c in signal_columns[1:]:
         df_raw[c] = quantile_normalize_using_target(df_raw[c],
-                                                    df_raw[df_raw.columns[0]])
+                                                    df_raw[signal_columns[0]])
     df_raw.to_csv(path, sep='\t')
     print('{} to {}'.format('Saved QUANTILE', path))
 
 
-def process_norm(path, data, sizes_path, peaks_sizes_path):
+def process_frip(path, data, sizes_path, peaks_sizes_path):
+    """Normalization based on library depths, Frips, and intersection fraction of given location with peaks"""
     sizes = pd.read_csv(sizes_path, sep='\t', names=('name', 'tags'), index_col='name')
     peaks_sizes = pd.read_csv(peaks_sizes_path, sep='\t', names=('name', 'tags_in_peaks'),
                               index_col='name')
 
     counts = pd.merge(sizes, peaks_sizes, left_index=True, right_index=True)
 
-    counts['input_tags'] = counts.loc['OD_input_unique_tags', 'tags']
-    counts['input_tags_in_peaks'] = counts.loc['OD_input_unique_tags', 'tags_in_peaks']
+    # Here we assume that we have single input for everything
+    input_column = [c for c in data.columns if is_input(c)][0]
+    counts['input_tags'] = counts.loc[input_column, 'tags']
+    counts['input_tags_in_peaks'] = counts.loc[input_column, 'tags_in_peaks']
 
     counts = counts[~ counts.index.str.contains("input")]
 
@@ -152,8 +137,7 @@ def process_norm(path, data, sizes_path, peaks_sizes_path):
 TMM_R_PATH = os.path.dirname(os.path.realpath(__file__)) + '/tmm.R'
 
 
-def process_diffbind_scores(data_path, data, sizes, post_process):
-    print('Processing DiffBind scores')
+def process_diffbind_scores(data_path, data, sizes, post_process_callback):
     od_input = [c for c in data.columns.values if is_od_input(c)][0]
     yd_input = [c for c in data.columns.values if is_yd_input(c)][0]
     ods = [c for c in data.columns if is_od(c)]
@@ -162,13 +146,13 @@ def process_diffbind_scores(data_path, data, sizes, post_process):
     scores, lib_sizes = process_scores(data, sizes, records)
     scores_path = re.sub('.tsv', '_scores.tsv', data_path)
     save_scores(scores_path, data, scores, 'diffbind scores')
-    post_process(scores_path)
+    post_process_callback(scores_path)
 
     tmm_results = process_tmm(scores, lib_sizes)
     scores_tmm = tmm_results * 10000000
     tmm_scores_path = re.sub('.tsv', '_scores_tmm.tsv', data_path)
     save_scores(tmm_scores_path, data, scores_tmm, 'diffbind TMM scores')
-    post_process(tmm_scores_path)
+    post_process_callback(tmm_scores_path)
 
 
 def process_tmm(data, lib_sizes):
@@ -205,7 +189,7 @@ https://docs.google.com/document/d/1zH5cw5Zal546xkoFFCVqhhYmf3742efhddz5cqpD9PQ/
         scale = 1
     if scale != 0:
         cont = math.ceil(cont * scale)
-    # According to pv.get_reads() function(utils.R:239), if reads number is < 1 than it should be 1
+    # According to DiffBind pv.get_reads() function (utils.R:239), if reads number is < 1 than it should be 1
     return max(1, cond - cont)
 
 
@@ -240,12 +224,12 @@ def main():
     print('LIBRARIES SIZES PATH', sizes_path)
     print('PEAKS SIZES PATH', peaks_sizes_path)
 
-    def post_process(path):
+    def post_process_callback(path):
         print('RESULT', path)
         signals_visualize.process(path)
         signals_tests.process(path)
 
-    process(data_path, sizes_path, peaks_sizes_path, post_process)
+    process(data_path, sizes_path, peaks_sizes_path, post_process_callback)
 
 
 if __name__ == "__main__":
