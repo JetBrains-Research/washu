4bf7ef02ff918857c014baea8d8c8876a34c945b
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2018-01-23 11:50:11 +0300

    1) r2 metric 2) code improvements

--- downstream/signals/signal_r2_permutation_test.py
+++ downstream/signals/signal_r2_permutation_test.py
@@ -5,10 +5,12 @@ import pandas as pd
 import numpy as np
 import numpy.random as rnd
 from multiprocessing import Pool
+from sklearn.metrics import r2_score
 from typing import List
 
 from downstream.aging import is_od, is_yd
 from downstream.signals.signals_visualize import pca_separation_fit_error, pca_signal
+from downstream.signals.pca_fit_error_permutation_test import collect_paths
 
 import matplotlib
 
@@ -16,11 +18,14 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt  # nopep8
 
 
-def _random_split_error(donors, x_r):
-    # Assume two groups: "group1" and "donors - group1"
-    gr1 = set(rnd.choice(donors, len(donors) // 2, replace=False))
-    y = [0 if d in gr1 else 1 for d in donors]
-    return pca_separation_fit_error(x_r, y)
+def _homogeneous_split(ods, yds):
+    gr1 = set()
+    for age_gr in [list(ods), list(yds)]:
+        group_size = len(age_gr) // 2
+        rnd.shuffle(age_gr)
+        gr1.update(age_gr[0:group_size])
+
+    return gr1
 
 
 def _process(path: Path, simulations: int, seed: int, threads: int, plot=True):
@@ -36,63 +41,66 @@ def _process(path: Path, simulations: int, seed: int, threads: int, plot=True):
     ##################################################
     # Make ODS, YDS groups even length for simplicity:
     ods = [c for c in df.columns if is_od(c)]
-    # rnd.shuffle(ods)
-    # ods = ods[0:2 * (len(ods) // 2)]
+    rnd.shuffle(ods)
+    ods = ods[0:2 * (len(ods) // 2)]
 
     yds = [c for c in df.columns if is_yd(c)]
-    # rnd.shuffle(yds)
-    # yds = yds[0:2 * (len(yds) // 2)]
+    rnd.shuffle(yds)
+    yds = yds[0:2 * (len(yds) // 2)]
 
     ##################################################
     # Signal DF:
     donors = sorted(ods + yds)
     signal = df.loc[:, donors]
 
-    pca, x_r = pca_signal(signal)
-    actual_error = pca_separation_fit_error(x_r,
-                                            [0 if is_yd(d) else 1 for d in donors])
-
     if threads == 1:
-        rnd_results = []
+        r2_list = []
+
         for i in range(simulations):
-            rnd_results.append(_random_split_error(donors, x_r))
+            r2_list.append(_homogeneous_split_r2(donors, ods, yds, signal))
     else:
         with Pool(processes=threads) as pool:
-            tasks = [pool.apply_async(_random_split_error, (donors, x_r)) for _i in
+            tasks = [pool.apply_async(_homogeneous_split_r2, (donors, ods, yds, signal)) for _i in
                      range(simulations)]
-            rnd_results = [task.get(timeout=timeout_secs) for task in tasks]
+            r2_list = [task.get(timeout=timeout_secs) for task in tasks]
 
-    # hack not to get zero pvalue
-    rnd_results.append(actual_error)
-    rr = np.asarray(rnd_results)
-    pvalue = np.sum(rr <= actual_error) / len(rr)
+    rr = np.asarray(r2_list)
+    r2_mean = np.mean(rr)
+    r2_median = np.median(rr)
 
-    print("[ACTUAL]: {}, [SIMUL]: [min, max] = [{}, {}], [2%, 98%] = [{}, {}]; 50% = {}, "
-          "p-value: {}".format(actual_error,
-                               np.min(rr[0:len(rr) - 1]), np.max(rr[0:len(rr) - 1]),
-                               np.percentile(rr, 2), np.percentile(rr, 98), np.percentile(rr, 50),
-                               pvalue
-                               )
-          )
+    print("mean = {}, median = {}, [min, max] = [{}, {}], [2%, 98%] = [{}, {}]".format(
+        r2_mean, r2_median, np.min(rr), np.max(rr),
+        np.percentile(rr, 2), np.percentile(rr, 98))
+    )
 
     if plot:
         plt.hist(rr)
-        plt.title("Pvalue for {} random groups = {}".format(len(rr), pvalue))
-        plt.axvline(x=actual_error, color="red", label="ODS vs YDS error")
-        plt.xlabel("PCA classification error")
+        plt.xlim(xmin=0, xmax=1)
+        plt.title("R2 for {} hom-groups. Mean = {:.2f}, Median = {:.2f}".format(len(rr), r2_mean, r2_median))
+        plt.axvline(x=r2_mean, color="red", label="R2 mean")
+        plt.axvline(x=r2_median, color="green", label="R2 median")
+        plt.xlabel("Each locus R2 for mean signal @ group1 vs group2")
         plt.legend()
 
-        plt.savefig(str(path.with_suffix(".permutations.pvalue.png")))
+        plt.savefig(str(path.with_suffix(".permutations.r2.png")))
         plt.close()
 
-    return pvalue
+    return r2_mean, r2_median
+
+
+def _homogeneous_split_r2(donors: List, ods: List, yds: List, signal: pd.DataFrame):
+    gr1 = _homogeneous_split(ods, yds)
+    gr1_means = signal.loc[:, gr1].mean(axis=1)
+    gr2_means = signal.loc[:, list(set(donors) - gr1)].mean(axis=1)
+    r2 = r2_score(gr1_means, gr2_means)
+    return r2
 
 
 def _cli():
     parser = argparse.ArgumentParser(
-        description="For each given normalised signal file calculates pvalue for H0: pca "
-                    "separation fit error for given ODS & YDS separation doesn't differ from  "
-                    "random donors split into 2 groups.",
+        description="For each given normalised signal file calculates r2 distribution for "
+                    "donors random split in 2 groups, containing same fraction of old and "
+                    "young donors.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("path",
@@ -102,7 +110,7 @@ def _cli():
                         type=int, default=4)
     parser.add_argument('-n', help="Simulations number to calculated pvalue", type=int,
                         default=100*1000)
-    #
+
     args = parser.parse_args()
     root = Path(args.path)
     seed = args.seed
@@ -118,7 +126,7 @@ def _cli():
     for i, path in enumerate(paths, 1):
         print("--- [{} / {}] -----------".format(i, n_paths))
         print("Process:", path)
-        pvalue = _process(path, simulations=simulations, seed=seed, threads=threads)
+        r2_mean, r2_median = _process(path, simulations=simulations, seed=seed, threads=threads)
 
         norm = re.sub('(.*_)|(\\.tsv$)', '', path.name)
         matches = re.match(".*/(H\\w*)/.*", str(path), re.IGNORECASE)
@@ -126,35 +134,27 @@ def _cli():
             mod = matches.group(1)
         else:
             mod = "N/A"
-        records.append((mod, str(path.parent), norm, pvalue))
+        records.append((mod, str(path.parent), norm, r2_mean, r2_median))
 
-    # sort by pvalue
-    records.sort(key=lambda v: v[-1])
+    # sort by (median, mean)
+    # records.sort(key=lambda v: v[-2:])
+    # sort by (mod, hist, norm)
+    records.sort(key=lambda v: (v[:3]))
 
     if len(paths) > 1:
         print("====================")
-        for i, (_mod, path, _norm, pvalue) in enumerate(records, 1):
-            print("{}. {}: {}".format(i, pvalue, path))
+        for i, (_mod, path, norm, r2_mean, r2_median) in enumerate(records, 1):
+            print("{}. mean = {}, median = {} : [{}] {}".format(i, r2_mean, r2_median, norm, path))
 
         df = pd.DataFrame.from_records(
             records,
-            columns=["modification", "file", "normalization", "pvalue"]
+            columns=["modification", "file", "normalization", "mean", "median"]
         )
         # table: mod, folder, norm, error
-        out = str(root / "report.permutation_pvalue.csv")
+        out = str(root / "report.permutation_r2.csv")
         df.to_csv(out, index=None)
 
-        print("P-values table saved to:", out)
-
-
-def collect_paths(root: Path) -> List[Path]:
-    if root.is_dir():
-        # filter normalized signal files:
-        paths = [p for p in root.glob("**/*.tsv") if p.name.startswith(p.parent.name + "_")]
-        paths = [p for p in paths if not p.name.endswith(".bed.tsv")]
-    else:
-        paths = [root]
-    return paths
+        print("R2 distribution features saved to:", out)
 
 
 if __name__ == "__main__":
