ce5efbbc33853e83248f049c15c83384d556e688
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2018-02-05 14:14:45 +0300

    Updated report script to the new layout : modification/fragment/regions_id

--- downstream/signals/signals_report.sh
+++ downstream/signals/signals_report.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Script to create report for signals
+# Output example:
+# H3K27me3  150  cpg_minavcov10_complex_4outliers.narrow.adjusted.regions.filtered  rawz                              2
+# H3K27ac   150  cpg_minavcov10_complex_4outliers.narrow.adjusted.regions.filtered  diffbind_tmm_minus_full           3
 # author oleg.shpynov@jetbrains.com
 
 >&2 echo "signals_report.sh $@"
@@ -12,13 +15,14 @@ WORK_DIR=$1
 
 cd $WORK_DIR
 T=$'\t';
-echo "modification${T}file${T}normalization${T}e"
+echo "modification${T}fragment${T}file${T}normalization${T}e"
 for F in $(find . -name "*_pca_fit_error.csv"); do
     IFS='/' read -ra CHUNKS <<< "$F"
     MODIFICATION=${CHUNKS[1]}
-    FOLDER=${CHUNKS[2]}
-    FILE=${CHUNKS[3]}
-    NORMALIZATION=$(echo "$FILE" | sed 's#_pca_fit_error.csv##g' | sed "s#${FOLDER}_##g")
+    FRAGMENT=${CHUNKS[2]}
+    FOLDER=${CHUNKS[3]}
+    FILE=${CHUNKS[4]}
+    NORMALIZATION=$(echo "$FILE" | sed 's#_pca_fit_error.csv##g' | sed "s#${FOLDER}_##g" | sed "s#.*\]_##g")
     ERROR=$(cat $F)
-    echo "$MODIFICATION$T$FOLDER$T$NORMALIZATION$T$ERROR"
+    echo "$MODIFICATION$T$FRAGMENT$T$FOLDER$T$NORMALIZATION$T$ERROR"
 done
