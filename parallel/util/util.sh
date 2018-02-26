7c7059b2473b85f273203569847a52b6abb380a8
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2018-02-26 15:06:02 +0300

    Slash char in tmpdir fixed (washu local runs tmp folder are not cleared #46)

--- parallel/util/util.sh
+++ parallel/util/util.sh
@@ -99,9 +99,9 @@ check_logs()
 
 job_tmp_dir() {
     if [ -z "${PBS_JOBID}" ]; then
-      TMP_DIR=~/tmp/job$$;
+      TMP_DIR=~/tmp/job$$/;
     else
-      TMP_DIR="/tmp/$PBS_JOBID";
+      TMP_DIR="/tmp/$PBS_JOBID/";
     fi
     mkdir -p "${TMP_DIR}"
 
