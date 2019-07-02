bc46902187aec167ea471d56b5a86fff555f347b
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2019-07-02 18:06:57 +0300

    Cleanup

--- downstream/diff/run_manorm.sh
+++ downstream/diff/run_manorm.sh
@@ -55,44 +55,6 @@ echo "$INPUTS_O"
 # Configuration end ############################################################
 ################################################################################
 
-DIFF_MACS_POOLED="${PREFIX}_macs_pooled"
-echo
-echo "Processing $DIFF_MACS_POOLED"
-if [ ! -d $DIFF_MACS_POOLED ]; then
-    mkdir -p ${DIFF_MACS_POOLED}
-    cd ${DIFF_MACS_POOLED}
-    echo "Processing MACS2 pooled peaks and compare them";
-
-    run_parallel << SCRIPT
-#!/bin/sh
-#PBS -N ${NAME}_Y_macs2_broad_${BROAD_CUTOFF}
-#PBS -l nodes=1:ppn=8,walltime=24:00:00,vmem=16gb
-#PBS -j oe
-#PBS -o ${DIFF_MACS_POOLED}/${NAME}_Y_macs2_broad_${BROAD_CUTOFF}.log
-# This is necessary because qsub default working dir is user home
-cd ${DIFF_MACS_POOLED}
-macs2 callpeak -t $READS_Y -c $INPUTS_Y -f BAM -g hs -n Y_${BROAD_CUTOFF} -B --broad --broad-cutoff ${BROAD_CUTOFF}
-SCRIPT
-    QSUB_ID1=$QSUB_ID
-
-    run_parallel << SCRIPT
-#!/bin/sh
-#PBS -N ${NAME}_O_macs2_broad_${BROAD_CUTOFF}
-#PBS -l nodes=1:ppn=8,walltime=24:00:00,vmem=16gb
-#PBS -j oe
-#PBS -o ${DIFF_MACS_POOLED}/${NAME}_O_macs2_broad_${BROAD_CUTOFF}.log
-# This is necessary because qsub default working dir is user home
-cd ${DIFF_MACS_POOLED}
-macs2 callpeak -t $READS_O -c $INPUTS_O -f BAM -g hs -n O_${BROAD_CUTOFF} -B --broad --broad-cutoff ${BROAD_CUTOFF}
-SCRIPT
-    QSUB_ID2=$QSUB_ID
-    wait_complete "$QSUB_ID1 $QSUB_ID2"
-
-    check_logs
-    bash ${WASHU_ROOT}/bed/compare.sh Y_${BROAD_CUTOFF}_peaks.broadPeak O_${BROAD_CUTOFF}_peaks.broadPeak ${NAME}_${BROAD_CUTOFF}
-fi
-
-
 # MANorm
 MANORM="${PREFIX}_manorm"
 echo
